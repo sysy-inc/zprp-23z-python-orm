8d321c72f12603a64cc9079dbdfaa148b038d66d
xDepcio <olek.drwal@gmail.com>
2024-03-19 00:34:10 +0100

    added types for create_table method in SQLite Adapter

--- migration_engine/adapters/sqlite3_adapter.py
+++ migration_engine/adapters/sqlite3_adapter.py
@@ -1,18 +1,17 @@
-from typing import Literal
+from typing import Literal, TypedDict
 
 from base_adapter import BaseAdapter
 
 
 class SQLite3Adapter(BaseAdapter):
-    type data_types = Literal["TEXT", "INTEGER", "REAL", "BLOB", "NULL"]
-    type constraints = Literal["PRIMARY KEY", "UNIQUE", "NOT NULL", "DEFAULT"]
+    type DataTypes = Literal["TEXT", "INTEGER", "REAL", "BLOB", "NULL"]
+    type Constraints = Literal["PRIMARY KEY", "UNIQUE", "NOT NULL", "DEFAULT"]
+    Column = TypedDict(
+        "Column", {"name": str, "data_type": DataTypes, "constraints": Constraints}
+    )
 
     def __init__(self):
         pass
 
-    def create_table(self, table_name: str, columns):
-        return super().create_table(table_name, columns)
-
-
-SQLite3Adapter()
-print("x")
+    def create_table(self, table_name: str, columns: list[Column]):
+        pass
