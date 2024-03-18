0503817ad31ebe17aa5d952ef3693309c2d039ed
xDepcio <olek.drwal@gmail.com>
2024-03-19 00:20:49 +0100

    added basic sqlite3 adapter

---
+++ migration_engine/sqlite3_adapter.py
@@ -0,0 +1,17 @@
+from typing import Literal
+
+from migration_engine.base_adapter import BaseAdapter
+
+
+class SQLite3Adapter(BaseAdapter):
+    type data_types = Literal["TEXT", "INTEGER", "REAL", "BLOB", "NULL"]
+    type constraints = Literal["PRIMARY KEY", "UNIQUE", "NOT NULL", "DEFAULT"]
+
+    def __init__(self):
+        pass
+
+    def create_table(self, table_name: str, columns):
+        return super().create_table(table_name, columns)
+
+
+SQLite3Adapter()
