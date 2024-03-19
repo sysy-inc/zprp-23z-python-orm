9b4300440deffca3ce7b6bf9f4802c68055054ac
xDepcio <olek.drwal@gmail.com>
2024-03-19 01:18:22 +0100

    added example implementation of Table class utilizing Adapters

--- migration_engine/migrator.py
+++ migration_engine/migrator.py
@@ -14,7 +14,17 @@ class BaseTable:
             table_instance.adapter.execute_migration()
 
 
+# Example of how Table class and classes modeling DB schema can be implemented
 class Table(BaseTable):
+    """
+    This is example of how Table class can be implemented
+    Key notes, that have to be taken into account:
+    - Table class HAVE to inherit from BaseTable and BaseTable MUST only have one class inheriting from it
+    - Table class HAVE to have adapter attribute, which is instance of BaseAdapter or its child
+    """
+
+    adapter: BaseAdapter
+
     def __init__(self):
         self.adapter = SQLite3Adapter()
 
