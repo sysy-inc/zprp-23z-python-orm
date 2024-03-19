3a037b973f735e34ec289a429610e29b2675e08a
xDepcio <olek.drwal@gmail.com>
2024-03-19 23:11:07 +0100

    moved current interface implementation to pdm's src/

--- src/zprp_23z_python_orm/migration_engine/migration_element.py
+++ src/zprp_23z_python_orm/migration_engine/migration_element.py
@@ -1,13 +1,15 @@
 from zprp_23z_python_orm.migration_engine.adapters.base_adapter import BaseAdapter
 
 
-class BaseTable:
+# All classes need to inherit from this class if they participate in a migration
+class MigrationElement:
     adapter: BaseAdapter
 
     @classmethod
     def migrate(cls):
-        table = BaseTable.__subclasses__()
+        table = MigrationElement.__subclasses__()
         for cls in table:
-            print(cls.__name__, cls.__dict__)
+            # Do migration action
+            # print(cls.__name__, cls.__dict__)
             table_instance = cls()
             table_instance.adapter.execute_migration()
