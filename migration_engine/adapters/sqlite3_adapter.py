c5821aa7a24735048ad8438d67d31aaa3832b33b
Jed Grabski <jedrzej@grabski.pl>
2024-03-19 21:43:09 +0100

    Changed adapter structure.
    Adapter will now implement dataclasses representing objects in an adapted database.
    
    New dataclasses will be added.

--- migration_engine/adapters/sqlite3_adapter.py
+++ migration_engine/adapters/sqlite3_adapter.py
@@ -1,24 +1,32 @@
+from __future__ import annotations
 from typing import Literal, TypedDict
-
+from dataclasses import dataclass
 from adapters.base_adapter import BaseAdapter
 
 
+type DataTypes = Literal["TEXT", "INTEGER", "REAL", "BLOB", "NULL"]
+type Constraints = Literal["PRIMARY KEY", "UNIQUE", "NOT NULL", "DEFAULT"]
+
+
+@dataclass
 class SQLite3Adapter(BaseAdapter):
-    type DataTypes = Literal["TEXT", "INTEGER", "REAL", "BLOB", "NULL"]
-    type Constraints = Literal["PRIMARY KEY", "UNIQUE", "NOT NULL", "DEFAULT"]
-    Column = TypedDict(
-        "Column", {"name": str, "data_type": DataTypes, "constraints": Constraints}
-    )
-    Table = TypedDict("Table", {"name": str, "columns": list[Column]})
+    
+
+    @dataclass
+    class Column:
+        name: str
+        data_type: DataTypes
+        constraints: list[Constraints]
+
+    @dataclass
+    class Table:
+        name: str
+        columns: list[SQLite3Adapter.Column]
+
     tables: list[Table] = []
 
     def __init__(self):
         pass
 
-    def create_table(self, table_name: str, columns: list[Column]):
-        self.tables.append({"name": table_name, "columns": columns})
-
-    def execute_migration(self):
-        print(self.tables)
-        print("Migration executed")
-        pass
+    def create_table(self, table: Table):
+        self.tables.append(table)
