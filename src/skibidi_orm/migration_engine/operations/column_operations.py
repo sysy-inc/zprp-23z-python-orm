b1c730f79dc3384d12eb918cff6499ea593d0bda
maksym <01178511@pw.edu.pl>
2024-03-30 21:54:33 +0100

    make column operations dataclasses, add content type declarations based on operation type

--- src/skibidi_orm/migration_engine/operations/column_operations.py
+++ src/skibidi_orm/migration_engine/operations/column_operations.py
@@ -1,5 +1,7 @@
 from abc import ABC
 from skibidi_orm.migration_engine.operations.operation_type import OperationType
+from skibidi_orm.migration_engine.operations.constraints import Constraint
+from dataclasses import dataclass
 
 
 class ColumnOperation(ABC):
@@ -7,8 +9,11 @@ class ColumnOperation(ABC):
 
     isReversible: bool
     operation_type: OperationType
+    table_name: str
+    column_name: str
 
 
+@dataclass(frozen=True)
 class AddColumn(ColumnOperation):
     """Class for adding a column"""
 
@@ -16,6 +21,7 @@ class AddColumn(ColumnOperation):
     isReversible = True
 
 
+@dataclass(frozen=True)
 class DeleteColumn(ColumnOperation):
     """Class for deleting a column"""
 
@@ -23,15 +29,51 @@ class DeleteColumn(ColumnOperation):
     isReversible = False
 
 
+@dataclass(frozen=True)
 class RenameColumn(ColumnOperation):
     """Class for renaming a column"""
 
     operation_type = OperationType.RENAME
     isReversible = True
 
+    new_name: str
 
-class AlterColumn(ColumnOperation):
-    """Class for altering a column - changing its type or constraints"""
 
-    operation_type = OperationType.ALTER
-    isReversible = True  # TODO: Is this always true with modifying data types?
+@dataclass(frozen=True)
+class AddConstraint(ColumnOperation):
+    """Class for adding a constraint on a column"""
+
+    operation_type = OperationType.CONSTRAINT_CHANGE
+    isReversible = True
+
+    constraint: Constraint
+
+
+@dataclass(frozen=True)
+class DeleteConstraint(ColumnOperation):
+    """Class for deleting a constraint on a column"""
+
+    operation_type = OperationType.CONSTRAINT_CHANGE
+    isReversible = True
+
+    constraint: Constraint
+
+
+@dataclass(frozen=True)
+class ChangeConstraint(ColumnOperation):
+    """Class for changing a constraint on a column"""
+
+    operation_type = OperationType.CONSTRAINT_CHANGE
+    isReversible = True
+
+    old_constraint: Constraint
+    new_constraint: Constraint
+
+
+@dataclass(frozen=True)
+class ChangeDataType(ColumnOperation):
+    """Class for changing the data type of a column"""
+
+    operation_type = OperationType.DTYPE_CHANGE
+    isReversible = False
+    new_dtype: str
