9d4a155a922cd07e6cb56b9d4d30362ce3f178bd
maksym <01178511@pw.edu.pl>
2024-03-30 22:07:59 +0100

    make table operations dataclasses, rename them

--- src/skibidi_orm/migration_engine/operations/table_operations.py
+++ src/skibidi_orm/migration_engine/operations/table_operations.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from skibidi_orm.migration_engine.operations.operation_type import OperationType
+from dataclasses import dataclass
 
 
 class TableOperation(ABC):
@@ -9,29 +10,25 @@ class TableOperation(ABC):
     isReversible: bool
 
 
-class CreateTable(TableOperation):
+@dataclass(frozen=True)
+class CreateTableOperation(TableOperation):
     """Class for creating a table"""
 
     operation_type = OperationType.CREATE
     isReversible = True
 
 
-class DeleteTable(TableOperation):
+@dataclass(frozen=True)
+class DeleteTableOperation(TableOperation):
     """Class for deleting a table"""
 
     operation_type = OperationType.DELETE
     isReversible = False
 
 
-class RenameTable(TableOperation):
+@dataclass(frozen=True)
+class RenameTableOperation(TableOperation):
     """Class for renaming a table"""
 
     operation_type = OperationType.RENAME
-    isReversible = True
-
-
-class AlterTable(TableOperation):
-    """Class for altering a table - adding or removing columns"""
-
-    operation_type = OperationType.ALTER
     isReversible = True
