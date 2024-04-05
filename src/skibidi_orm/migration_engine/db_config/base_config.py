5aa624a2d93021d26e5a03e5440aad51406b5293
xDepcio <olek.drwal@gmail.com>
2024-04-05 23:43:32 +0200

    Created seperate folder for db config

---
+++ src/skibidi_orm/migration_engine/db_config/base_config.py
@@ -0,0 +1,38 @@
+from typing import Any, Callable
+
+
+class ConfigSingleton(type):
+    _instances: dict[Any, Any] = {}
+
+    def __call__(cls, *args: Any, **kwargs: Any) -> Any:
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+    @staticmethod
+    def instance_exists(class_name: Any) -> bool:
+        return class_name in ConfigSingleton._instances
+
+
+class DbConfig:
+    __instances_count = 0
+
+    @classmethod
+    def instance(cls):
+        if not ConfigSingleton.instance_exists(cls):
+            raise ReferenceError("Instance does not exist")
+        return cls()
+
+    def __init_subclass__(cls) -> None:
+        cls.__init__ = DbConfig.__modify_init(cls.__init__)
+
+    @staticmethod
+    def __modify_init(init_func: Callable[[Any], Any]):
+        def wrapper(*args: Any, **kwargs: Any):
+            if DbConfig.__instances_count > 0:
+                raise RuntimeError("Only one instance of this class is allowed")
+            DbConfig.__instances_count += 1
+            init_func(*args, **kwargs)
+
+        return wrapper
