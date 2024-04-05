497a8d1dff793ae8ad73e825a021f099c29674b0
xDepcio <olek.drwal@gmail.com>
2024-04-06 00:23:12 +0200

    Added support for docstrings in pytest

--- tests/migration_engine/conftest.py
+++ tests/migration_engine/conftest.py
@@ -1,3 +1,5 @@
+from typing import Any
+from colorama import Style
 import pytest
 
 from skibidi_orm.migration_engine.db_config.base_config import (
@@ -10,3 +12,14 @@ from skibidi_orm.migration_engine.db_config.base_config import (
 def reset_config_singleton(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setattr(ConfigSingleton, "_instances", {})
     monkeypatch.setattr(BaseDbConfig, "_BaseDbConfig__instances_count", 0)
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item: Any) -> Any:
+    outcome: Any = yield
+    report = outcome.get_result()
+    test_fn = item.obj
+    docstring = getattr(test_fn, "__doc__")
+    if docstring:
+        docstring = docstring.strip()
+        report.nodeid = Style.DIM + docstring + Style.RESET_ALL
