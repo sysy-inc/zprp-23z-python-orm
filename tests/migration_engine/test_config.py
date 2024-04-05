fac3b64f62dd29b08b5bed1629bb075b4099d860
xDepcio <olek.drwal@gmail.com>
2024-04-06 00:25:37 +0200

    Added docstrings to some tests. Those docstrings will be shows in console when running `pdm run tests`

--- tests/migration_engine/test_config.py
+++ tests/migration_engine/test_config.py
@@ -5,6 +5,9 @@ from skibidi_orm.migration_engine.db_config.sqlite3_config import SQLite3Config
 
 
 def test_should_only_create_one_instance_of_each_config():
+    """
+    Test that only one instance of each config is created
+    """
     x = SQLite3Config(db_path="first_path")
     y = SQLite3Config(db_path="next_path")
     assert x is y
@@ -13,6 +16,9 @@ def test_should_only_create_one_instance_of_each_config():
 
 
 def test_should_raise_error_when_instantiating_other_config():
+    """
+    Should raise error when trying to instantiate other config after one has been instantiated
+    """
     SQLite3Config(db_path="first_path")
     with pytest.raises(RuntimeError) as exc_info:
         PostgresConfig(db_path="first_path")
@@ -20,6 +26,9 @@ def test_should_raise_error_when_instantiating_other_config():
 
 
 def test_accessing_through_instance_method_should_throw_error_when_instance_does_not_exist():
+    """
+    Should raise error when trying to access instance method when instance does not exist
+    """
     with pytest.raises(ReferenceError) as exc_info:
         SQLite3Config.instance()
     assert str(exc_info.value) == "Instance does not exist"
