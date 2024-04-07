17a0747869a271a391158fc7fc925efce9fc6a5c
xDepcio <olek.drwal@gmail.com>
2024-04-07 19:55:26 +0200

    added docs

--- src/skibidi_orm/migration_engine/db_inspectors/base_inspector.py
+++ src/skibidi_orm/migration_engine/db_inspectors/base_inspector.py
@@ -11,16 +11,28 @@ from skibidi_orm.migration_engine.adapters.base_adapter import (
 class BaseDbInspector(ABC):
     @abstractmethod
     def get_tables(self) -> list[BaseTable[BaseColumn[Any, Any]]]:
+        """
+        Get all tables from the database.
+        """
         pass
 
     @abstractmethod
     def get_tables_names(self) -> list[str]:
+        """
+        Get all tables names from the database.
+        """
         pass
 
     @abstractmethod
     def get_table_columns(self, table_name: str) -> list[BaseColumn[Any, Any]]:
+        """
+        Get all columns from a table.
+        """
         pass
 
     @abstractmethod
     def get_relations(self) -> list[Relation]:
+        """
+        Get all relations from the database.
+        """
         pass
