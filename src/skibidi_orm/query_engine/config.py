ff43c1fea0a89c55d8bbe418b43e74334e1b01e8
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2024-04-11 14:08:31 +0200

    create config class specific for sqlite database

--- src/skibidi_orm/query_engine/config.py
+++ src/skibidi_orm/query_engine/config.py
@@ -3,12 +3,15 @@ Module handles configuration data (needed to open connection) for different data
 """
 from abc import ABC, abstractmethod
 from typing import Any
+from skibidi_orm.query_engine.adapter.sqlite_adapter import SQLiteAdapter
 
 
 class Config(ABC):
     """
-    Abstract Base class for configuration
+    Abstract base class for configuration
     Stores all of data needed to connect to database
+
+    Subclasses must implement the __init__()
     """
     @abstractmethod
     def __init__(self, **kwargs: dict[str, Any]) -> None:
@@ -20,3 +23,21 @@ class Config(ABC):
         Return correct adapter for this database
         """
         return self._adapter
+
+
+class SQLiteConfig(Config):
+    """
+    Configuration class for database sqlite
+    """
+    def __init__(self, **kwargs: dict[str, Any]) -> None:
+        """
+        Create instance of SQLiteConfig
+        :param kwargs: keyword arguments to specify database configuration
+            - path(str): path to sqlite database
+
+        Example
+        SQLiteConfig(path='database.db)
+        """
+        super().__init__(**kwargs)
+        self._database_path = kwargs.get('path', '')
+        self._adapter = SQLiteAdapter()
