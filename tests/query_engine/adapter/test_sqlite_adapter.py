b2c48d686a4b758b8706bc0aac40b1a53985906a
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2024-04-11 21:48:34 +0200

    create tests for sqlite adapter

---
+++ tests/query_engine/adapter/test_sqlite_adapter.py
@@ -0,0 +1,23 @@
+from skibidi_orm.query_engine.adapter.sqlite_adapter import SQLiteAdapter
+import sqlite3
+
+
+def test_create_SQLiteAdapter():
+    adapter = SQLiteAdapter()
+    assert adapter.connector is None
+
+
+def test_make_connection():
+    adapter = SQLiteAdapter()
+    connection = adapter.make_connection(path='test.db')
+    assert connection is not None
+    assert isinstance(connection, sqlite3.Connection)
+
+
+def test_import_connector():
+    adapter = SQLiteAdapter()
+    assert adapter.connector is None
+    # import_connector is private so it can't be used here directly
+    # it is used during making connection
+    adapter.make_connection(path='test.db')
+    assert adapter.connector == sqlite3
