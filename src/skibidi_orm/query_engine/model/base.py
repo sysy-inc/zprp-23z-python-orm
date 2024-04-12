4d8ee2da4b1cbef9f1a0a89a4614352c193bd34e
Maria Polakowska <01169227@pw.edu.pl>
2024-04-12 22:00:52 +0200

    Add basis of __new__ method in MetaModel class

--- src/skibidi_orm/query_engine/model/base.py
+++ src/skibidi_orm/query_engine/model/base.py
@@ -2,17 +2,25 @@
 
 from pydantic import BaseModel
 from pydantic.main import ModelMetaclass
-
+from typing import Any, Tuple, Dict
 
 
 
 class MetaModel(ModelMetaclass):
     """ A metaclass for model """
-    pass
+    def __new__(cls, name: str, bases: Tuple[Any], attrs: Dict[str, Any]) -> Any:
+        super_new: Any = super().__new__ # type: ignore
+        # TODO check if name is correct
+        # TODO check if attrs' names are correct
+        # TODO check if is primary key
+        # TODO check if only one attr is primary key
+        # TODO check if atrr db_column is unique
+        return super_new(cls, name, bases, attrs)
 
 
 
-class Model(BaseModel, metaclass=MetaModel):
+
+class Model(BaseModel):
     """ A class to create your own database table """
     # TODO add information about inheritance
     pass
