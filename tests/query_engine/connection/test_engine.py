ce798ba72ed8a20a25747e7f046bbc47bd45e805
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2024-04-13 16:33:20 +0200

    change tests for engine and session so it handles reading config from file

--- tests/query_engine/connection/test_engine.py
+++ tests/query_engine/connection/test_engine.py
@@ -1,24 +1,31 @@
 from skibidi_orm.query_engine.config import SQLiteConfig
+from skibidi_orm.query_engine import config
 from skibidi_orm.query_engine.connection.engine import Engine
+import pytest
 
 
-def test_create_engine():
-    config = SQLiteConfig(path='test.db')
-    engine = Engine(config)
+@pytest.fixture
+def mock_get_configuration(monkeypatch):
+    def mock_config_data():
+        return SQLiteConfig(path='test.db')
+
+    monkeypatch.setattr(config, 'get_configuration', mock_config_data)
+
+
+def test_create_engine(mock_get_configuration):
+    engine = Engine()
     assert engine.connected is False
 
 
-def test_connect():
-    config = SQLiteConfig(path='test.db')
-    engine = Engine(config)
+def test_connect(mock_get_configuration):
+    engine = Engine()
     connection = engine.connect()
     assert connection is not None
     assert engine.connected is True
 
 
-def test_close():
-    config = SQLiteConfig(path='test.db')
-    engine = Engine(config)
+def test_close(mock_get_configuration):
+    engine = Engine()
     engine.connect()
     assert engine.connected is True
     engine.close()
--- tests/query_engine/connection/test_session.py
+++ tests/query_engine/connection/test_session.py
@@ -1,26 +1,33 @@
 from skibidi_orm.query_engine.config import SQLiteConfig
 from skibidi_orm.query_engine.connection.engine import Engine
 from skibidi_orm.query_engine.connection.session import Session
+from skibidi_orm.query_engine import config
+import pytest
 
 
-def test_create_session():
-    config = SQLiteConfig(path='test.db')
-    engine = Engine(config)
+@pytest.fixture
+def mock_get_configuration(monkeypatch):
+    def mock_config_data():
+        return SQLiteConfig(path='test.db')
+
+    monkeypatch.setattr(config, 'get_configuration', mock_config_data)
+
+
+def test_create_session(mock_get_configuration):
+    engine = Engine()
     session = Session(engine)
     assert session.connection is None
 
 
-def test_enter_exit():
-    config = SQLiteConfig(path='test.db')
-    engine = Engine(config)
+def test_enter_exit(mock_get_configuration):
+    engine = Engine()
     with Session(engine) as session:
         assert session.connection is not None
     assert session.connection is None
 
 
-def test_close():
-    config = SQLiteConfig(path='test.db')
-    engine = Engine(config)
+def test_close(mock_get_configuration):
+    engine = Engine()
     with Session(engine) as session:
         session.close()
     assert session.connection is None
