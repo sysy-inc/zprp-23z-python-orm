18290bbb1f2ace590433d74aff882a8be08ee85e
jedrzej-grabski <jedrzej@grabski.pl>
2024-04-14 13:46:35 +0200

    Altered the MigrationElement base class to allow for proper testing

--- src/skibidi_orm/migration_engine/migration_element.py
+++ src/skibidi_orm/migration_engine/migration_element.py
@@ -1,9 +1,11 @@
 from skibidi_orm.migration_engine.adapters.base_adapter import BaseAdapter
+from typing import Any
 
 
 # All classes need to inherit from this class if they participate in a migration
 class MigrationElement:
     adapter: BaseAdapter
+    operations: list[Any] = []
 
     @classmethod
     def migrate(cls):
@@ -13,3 +15,4 @@ class MigrationElement:
             # print(cls.__name__, cls.__dict__)
             table_instance = cls()
             table_instance.adapter.execute_migration()
+            MigrationElement.operations = table_instance.adapter.operation_list
