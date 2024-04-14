22ef602b5557c698cb30bd6dd23bb392a0f6c891
Magda Twarowska <mtwarowska02@gmail.com>
2024-04-14 16:07:01 +0200

    implemented fields & validators

--- src/skibidi_orm/query_engine/field/field.py
+++ src/skibidi_orm/query_engine/field/field.py
@@ -1,31 +1,79 @@
-from dataclasses import dataclass, field
-from typing import Any
+from skibidi_orm.query_engine.field.validators import (
+    MaxLengthValidator,
+    MaxValueValidator,
+    MinValueValidator,
+    DecimalValidator
+)
+from typing import Any, List, Type, Union
+import decimal
 
-@dataclass
+
+class NOT_PROVIDED:
+    """
+    Notes that no value has been passed to the 'default' attr in Field class
+    as 'None' could be considered as default value
+    """
+    pass
+
+
+class Error(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
 class Field:
-    null: bool = field(default=False, repr=False)
-    name: str = field(default="")            # python's field name
-    db_column: str = field(default="")        # column name in db
-    unique: bool = field(default=False, repr=False)
-    primary_key: bool = field(default=False, repr=False)
-    max_length: int = field(default=100, repr=False, metadata={"min_value": 0})
-    default = None                              #TODO do sth about it
-    validators: list[function] = field(default_factory=list, repr=False)  # list of validators to validate a type
-    field_type: str = field(default="")
+    instances_count: int = 0
 
+    def __init__(self, 
+                 null: bool = False,
+                 name: str = "", 
+                 db_column: str = "",
+                 unique: bool = False,
+                 primary_key: bool = False,
+                 max_length: Union[int, None] = None,       
+                 default: Any = NOT_PROVIDED,
+                 validators: List[object] = [],
+                 field_type: str = ""):
+        self.null = null,
+        self.name = name                # python's field name
+        self.db_column = db_column      # column name in db
+        self.unique = unique
+        self.primary_key = primary_key
+        self.max_length = max_length
+        self.default = default                        
+        self.validators = validators    # list of validators to validate a value; this collects all validators from all subclasses instances
+        self.field_type = field_type
+
+        self.instances_count = Field.instances_count
+        Field.instances_count += 1
+    
     def __str__(self):
         """
         Returns pythonic name of the field
         """
-        #TODO return also field model's name
+        # if hasattr(self, "model"):
+        #    return str(self.model), self.name
         return self.name
+
+    def get_type(self) -> str:
+        return ""
     
-    def deconstruct(self):
-        """
-        Returns essential data for the field to be recreated from file
-        ie name, list of positional args, list of keyword args, import path of the field
-        """
-        pass
+    def __lt__(self, other: Any) -> bool:
+        return self.instances_count < other.instances_count
+    
+    def __eq__(self, other: Any) -> bool:
+        return self.instances_count == other.instances_count
+    
+    def set_attr_names(self, name: str):
+        self.name = self.name or name
+        self.column = self.db_column or self.name
+    
+    def contribute_to_class(self, cls: Type[BaseException], name: str):     # change class here!!
+        self.set_attr_names(name)
+        self.model = cls
+        cls._meta.add_field(self)           # type: ignore
+        if self.column:
+            setattr(cls, self.name, 1)
 
     def to_python(self, value: Any):
         """
@@ -34,25 +82,225 @@ class Field:
         """
         return value
     
-    def db_type(self, conn_type: str):
+    def db_type(self, conn: dict[str, str]) -> object:
         """
         Returns database column type for this field for given connection from CONVERTER DICTIONARY
         """
-        pass
+        try:
+            column_type = conn[self.get_type()]
+        except KeyError:
+            return None
+        return column_type
 
-    def to_db_type(self, value:Any, conn_type: str):
+    def to_db_type(self, value: Any, conn_type: str):
         """
         Prepares the value to be saved into database
         """
+        # TODO implement functionality
         pass
-        #TODO implement functionality
 
-    def validate(self, **kwargs):
+    def check(self, **kwargs: Any):      
         """
-        Returns list of basic validators that need to be checked for every field type. 
+        Calls basic validators that need to be checked for every field type. 
         Specific validators nedd to be added in overriden methods
-        ie checking if field name does not have any illegal chars
         """
         pass
 
-    #TODO implement validators & errors 
+
+class IntegerField(Field):
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        self.add_validators()
+        
+    def get_type(self) -> str:
+        return "IntegerField"
+    
+    def to_python(self, value: Any):
+        if value is None:
+            return value
+        else:
+            try:
+                return int(value)
+            except (ValueError, TypeError):
+                raise Error("Value must be an integer")
+    
+    def add_validators(self):
+        # TODO optimize it
+        min_value, max_value = integer_field_range(self.get_type())
+        self.validators.append(MaxValueValidator(max_value))        
+        self.validators.append(MinValueValidator(min_value))
+
+
+class BigIntegerField(IntegerField):
+    def get_type(self) -> str:
+        return "BigIntegerField"
+
+
+class SmallIntegerField(IntegerField):
+    def get_type(self) -> str:
+        return "SmallIntegerField"
+
+
+class PositiveIntegerField(IntegerField):
+    def get_type(self) -> str:
+        return "PositiveIntegerField"
+
+
+class PositiveBigIntegerField(BigIntegerField):
+    def get_type(self) -> str:
+        return "PositiveBigIntegerField"
+
+
+class PositiveSmallIntegerField(SmallIntegerField):
+    def get_type(self) -> str:
+        return "PositiveSmallIntegerField"
+
+
+class DecimalField(Field):
+    def __init__(self, max_digits: int, decimal_places: int, **kwargs: Any):   
+        super().__init__(**kwargs)                                                    
+        self.decimal_places = decimal_places
+        self.max_digits = max_digits
+        self.validators.append(DecimalValidator(self.max_digits, self.decimal_places))
+    
+    def get_name(self) -> str:
+        return "DecimalField"
+
+    def check(self, **kwargs: Any):
+        super().check(**kwargs)
+        self._check_max_digits()
+        self._check_decimal_places()
+        self._check_max_digits_and_decimal_places()
+
+    def _check_max_digits(self):
+        msg = ""
+        try:
+            max_digits = int(self.max_digits)
+            if max_digits <= 0:
+                msg = "max_digits must be an integer greater than 0."
+                raise ValueError()
+        except (ValueError, TypeError):
+            if len(msg) == 0:
+                msg = "DecimalField must define max_digits attribute"
+            raise Error(msg)
+
+    def _check_decimal_places(self):
+        msg = ""
+        try:
+            decimal_places = int(self.decimal_places)
+            if decimal_places < 0:
+                msg = "decimal_places must be an integer greater or equal to 0."
+                raise ValueError()
+        except (TypeError, ValueError):
+            if len(msg) == 0:
+                msg = "DecimalField must define decimal_places attribute"
+            raise Error(msg)
+
+    def _check_max_digits_and_decimal_places(self):
+        if int(self.decimal_places) > int(self.max_digits):
+            raise Error("decimal_places must be greater or equal to max_digits.")
+    
+    def to_python(self, value: Any):
+        if value is None:
+            return value
+        try:
+            decimal_value = decimal.Decimal(value)
+        except (decimal.InvalidOperation, TypeError, ValueError):
+            raise Error("Value must be a decimal.")
+        if not decimal_value.is_finite():
+            raise Error("Value must be a decimal.")
+        return decimal_value
+
+
+class FloatField(Field):
+    def get_name(self) -> str:
+        return "FloatField"
+    
+    def to_python(self, value: Any):
+        if value is None:
+            return value
+        else:
+            try:
+                return float(value)
+            except (ValueError, TypeError):
+                raise Error("Value must be an float")
+
+
+class CharField(Field):
+    def __init__(self, *args: Any, **kwargs: Any):   
+        super().__init__(*args, **kwargs)   
+        if self.max_length is not None:     
+            self.validators.append(MaxLengthValidator(self.max_length))
+
+    def get_name(self) -> str:
+        return "CharField"
+
+    def to_python(self, value: Any) -> object:
+        if isinstance(value, str) or value is None:
+            return value
+        return str(value)
+    
+    def check(self, **kwargs: Any):
+        super().check(**kwargs)
+        self._check_max_length()
+    
+    def _check_max_length(self):
+        if self.max_length is not None:
+            if not isinstance(self.max_length, int) or self.max_length < 1:     # type: ignore
+                raise Error("max_length must be an integer greater than 0")
+
+
+class TextField(Field):
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+
+    def get_name(self) -> str:
+        return "TextField"
+
+    def to_python(self, value: Any):
+        if isinstance(value, str) or value is None:
+            return value
+        return str(value)
+
+
+class BooleanField(Field):
+    def get_name(self):
+        return "BooleanField"
+    
+    def to_python(self, value: Any):
+        if self.null and value in (None, "", [], {}):
+            return None
+        elif value in (True, False):
+            return bool(value)
+        elif value in ('1', 't', 'True'):
+            return True
+        elif value in ('0', 'f', 'False'):
+            return False
+        elif self.null:
+            raise Error("Invalid value (must be either True, False or None)")
+        else:
+            raise Error("Invalid value (must be either True or False)")
+
+
+class DateField(Field):
+    def get_name(self):
+        return "DateField"
+    # TODO add logic
+
+
+class DateTimeField(Field):
+    def get_name(self):
+        return "DateTimeField"
+    # TODO add logic
+
+
+class AutoField():
+    pass
+    # TODO add logic
+
+
+def integer_field_range(type: str) -> tuple[int, int]:
+    """
+    Function for mocking integer range from a database
+    """
+    return (1, 10)
---
+++ src/skibidi_orm/query_engine/field/validators.py
@@ -0,0 +1,89 @@
+from typing import Any
+import decimal
+
+
+class ValidationError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class BaseValidator:
+    def __init__(self, limit: int):
+        self.limit = limit
+        self.msg = f"Value is not meeting the criteria (should be equal to {self.limit})"
+
+    def __call__(self, value: Any):
+        if not self.compare(value, self.limit):
+            raise ValidationError(self.msg)
+
+    def compare(self, a: Any, b: Any) -> bool:
+        return a == b
+
+
+class MaxLengthValidator(BaseValidator):
+    def __init__(self, limit: int):
+        super().__init__(limit)
+        self.msg = f"Value is not meeting the criteria (length should be less than {self.limit})"
+
+    def compare(self, a: Any, b: Any) -> bool:
+        return a < b
+
+
+class MinLengthValidator(BaseValidator):
+    def __init__(self, limit: int):
+        super().__init__(limit)
+        self.msg = f"Value is not meeting the criteria (length should be more than {self.limit})"
+
+    def compare(self, a: Any, b: Any) -> bool:
+        return a > b
+
+
+class MaxValueValidator(BaseValidator):
+    def __init__(self, limit: int):
+        super().__init__(limit)
+        self.msg = f"Value is not meeting the criteria (should be less than {self.limit})"
+
+    def compare(self, a: Any, b: Any) -> bool:
+        return a < b
+
+
+class MinValueValidator(BaseValidator):
+    def __init__(self, limit: int):
+        super().__init__(limit)
+        self.msg = f"Value is not meeting the criteria (should be more than {self.limit})"
+
+    def compare(self, a: Any, b: Any) -> bool:
+        return a > b
+
+
+class DecimalValidator:
+    def __init__(self, max_digits: int, decimal_places: int):
+        self.decimal_places = decimal_places
+        self.max_digits = max_digits
+
+    def __call__(self, value: decimal.Decimal):
+        digit_tuple, exponent = value.as_tuple()[1:]
+        if exponent in {"F", "n", "N"}:
+            raise ValidationError("Value must be a decimal.")
+        if isinstance(exponent, int) and exponent >= 0:                                       
+            digits = len(digit_tuple)
+            if digit_tuple != (0,):
+                digits += exponent                               
+            decimals = 0
+        else:
+            if isinstance(exponent, int) and abs(exponent) > len(digit_tuple):
+                digits = decimals = abs(exponent)
+            elif isinstance(exponent, int):
+                digits = len(digit_tuple)
+                decimals = abs(exponent)
+            else:
+                decimals = 0
+                digits = 0
+        whole_digits = digits - decimals
+
+        if self.max_digits and digits > self.max_digits:
+            raise ValidationError("Number of digits is excedeed.")
+        if self.decimal_places and decimals > self.decimal_places:
+            raise ValidationError("Number of decimals is excedeed.")
+        if whole_digits > (self.max_digits - self.decimal_places):
+            raise ValidationError("Number of digits before decimal is excedeed")
