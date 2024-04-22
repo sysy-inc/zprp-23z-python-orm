f91c2608c96ec7241f99f9565a6fd2df7d6d0fba
xDepcio <olek.drwal@gmail.com>
2024-04-23 01:25:13 +0200

    removed not used import

--- tests/migration_engine/test_sqlite3_executor.py
+++ tests/migration_engine/test_sqlite3_executor.py
@@ -1,6 +1,5 @@
 from skibidi_orm.migration_engine.db_config.sqlite3_config import SQLite3Config
 from skibidi_orm.migration_engine.db_inspectors.sqlite3_inspector import SqliteInspector
-from skibidi_orm.migration_engine.operations.constraints import PrimaryKeyConstraint
 from skibidi_orm.migration_engine.sql_executor.sqlite3_executor import SQLite3Executor
 
 
