6c0ac0038ea3f01464daa0409658e136a26874cd
jedrzej-grabski <jedrzej@grabski.pl>
2024-04-24 13:56:12 +0200

    Made SQL3 Executor functional

--- src/skibidi_orm/migration_engine/sql_executor/sqlite3_executor.py
+++ src/skibidi_orm/migration_engine/sql_executor/sqlite3_executor.py
@@ -1,6 +1,10 @@
 import sqlite3
 from skibidi_orm.migration_engine.db_config.sqlite3_config import SQLite3Config
 from skibidi_orm.migration_engine.sql_executor.base_sql_executor import BaseSQLExecutor
+from skibidi_orm.migration_engine.operations.column_operations import ColumnOperation
+from skibidi_orm.migration_engine.operations.table_operations import TableOperation
+
+from skibidi_orm.migration_engine.converters.sqlite3_converter import SQLite3Converter
 
 
 class SQLite3Executor(BaseSQLExecutor):
@@ -12,3 +16,15 @@ class SQLite3Executor(BaseSQLExecutor):
             cursor = conn.cursor()
             cursor.execute(sql)
             conn.commit()
+
+    @staticmethod
+    def execute_operations(operations: list[TableOperation | ColumnOperation]):
+        for operation in operations:
+            if isinstance(operation, TableOperation):
+                SQLite3Executor.execute_sql(
+                    SQLite3Converter.convert_table_operation_to_SQL(operation)
+                )
+            else:
+                SQLite3Executor.execute_sql(
+                    SQLite3Converter.convert_column_operation_to_SQL(operation)
+                )
