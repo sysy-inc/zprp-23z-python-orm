648539b75ba2a98513ae066ea182070aaf3a54b2
maksym <01178511@pw.edu.pl>
2024-04-24 09:52:59 +0200

    modify one of the executor tests to account for proper constraint representation

--- tests/migration_engine/test_sqlite3_executor.py
+++ tests/migration_engine/test_sqlite3_executor.py
@@ -1,5 +1,8 @@
 from skibidi_orm.migration_engine.db_config.sqlite3_config import SQLite3Config
 from skibidi_orm.migration_engine.db_inspectors.sqlite3_inspector import SqliteInspector
+from skibidi_orm.migration_engine.operations.constraints import (
+    PrimaryKeyConstraint,
+)
 from skibidi_orm.migration_engine.sql_executor.sqlite3_executor import SQLite3Executor
 
 
@@ -12,7 +15,7 @@ def test_execute_sql(make_database: str):
     assert table.name == "test_table"
     assert table.columns[0].name == "id"
     assert table.columns[0].data_type == "INTEGER"
-    assert table.columns[0].constraints == ["PRIMARY KEY"]
+    assert table.columns[0].constraints == [PrimaryKeyConstraint("test_table", "id")]
     assert table.columns[1].name == "name"
     assert table.columns[1].data_type == "TEXT"
     assert table.columns[1].constraints == []
