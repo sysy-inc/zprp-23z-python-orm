f3b4cb3ce5df6ef9660df552d71b596ec32f1892
jed <jed>
2024-05-16 21:46:54 +0200

    Added example testing schema

--- testing_schema.py
+++ testing_schema.py
@@ -1,175 +1,174 @@
-class User(Table):  # type: ignore
-    columns = [
-        SQLite3Adapter.Column(
-            name="user_id",
-            data_type="INTEGER",
-            constraints=[C.PrimaryKeyConstraint("User", "user_id")],
-        ),
-        SQLite3Adapter.Column(
-            name="user_name",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("User", "user_name")],
-        ),
-    ]
+# class User(Table):  # type: ignore
+#     columns = [
+#         SQLite3Adapter.Column(
+#             name="user_id",
+#             data_type="INTEGER",
+#             constraints=[C.PrimaryKeyConstraint("User", "user_id")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="user_name",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("User", "user_name")],
+#         ),
+#     ]
 
-    table = SQLite3Adapter.Table(name="User", columns=columns)
-
-
-class Post(Table):  # type: ignore
-    columns = [
-        SQLite3Adapter.Column(
-            name="post_id",
-            data_type="INTEGER",
-            constraints=[C.PrimaryKeyConstraint("Post", "post_id")],
-        ),
-        SQLite3Adapter.Column(
-            name="post_name",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("Post", "post_name")],
-        ),
-    ]
-
-    table = SQLite3Adapter.Table(name="Post", columns=columns)
-    
-    
-### DODANIE TABELI
-    
-class Comment(Table):  # type: ignore
-
-    columns = [
-        SQLite3Adapter.Column(
-            name="comment_id",
-            data_type="INTEGER",
-            constraints=[C.PrimaryKeyConstraint("Comment", "comment_id")],
-        ),
-        SQLite3Adapter.Column(
-            name="comment_name",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("Comment", "comment_name")],
-        ),
-    ]
-
-    table = SQLite3Adapter.Table(name="Comment", columns=columns)
+#     table = SQLite3Adapter.Table(name="User", columns=columns)
 
 
-### ZMIANA NAZWY
+# class Post(Table):  # type: ignore
+#     columns = [
+#         SQLite3Adapter.Column(
+#             name="post_id",
+#             data_type="INTEGER",
+#             constraints=[C.PrimaryKeyConstraint("Post", "post_id")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="post_name",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("Post", "post_name")],
+#         ),
+#     ]
 
-class NewNamePost(Table):  # type: ignore
-    columns = [
-        SQLite3Adapter.Column(
-            name="post_id",
-            data_type="INTEGER",
-            constraints=[C.PrimaryKeyConstraint("NewNamePost", "post_id")],
-        ),
-        SQLite3Adapter.Column(
-            name="post_name",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("NewNamePost", "post_name")],
-        ),
-    ]
-
-    table = SQLite3Adapter.Table(name="NewNamePost", columns=columns)r.Table(name="NewNamePost", columns=columns)
-    
-    
-### Dodanie kolumny
-
-        SQLite3Adapter.Column(
-            name="post_content",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("Post", "post_content")],
-        ),
+#     table = SQLite3Adapter.Table(name="Post", columns=columns)
 
 
-### POWROT
+# ### DODANIE TABELI
 
-class User(Table):  # type: ignore
-    columns = [
-        SQLite3Adapter.Column(
-            name="user_id",
-            data_type="INTEGER",
-            constraints=[C.PrimaryKeyConstraint("User", "user_id")],
-        ),
-        SQLite3Adapter.Column(
-            name="user_name",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("User", "user_name")],
-        ),
-    ]
+# class Comment(Table):  # type: ignore
 
-    table = SQLite3Adapter.Table(name="User", columns=columns)
+#     columns = [
+#         SQLite3Adapter.Column(
+#             name="comment_id",
+#             data_type="INTEGER",
+#             constraints=[C.PrimaryKeyConstraint("Comment", "comment_id")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="comment_name",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("Comment", "comment_name")],
+#         ),
+#     ]
 
-
-class Post(Table):  # type: ignore
-    columns = [
-        SQLite3Adapter.Column(
-            name="post_id",
-            data_type="INTEGER",
-            constraints=[C.PrimaryKeyConstraint("Post", "post_id")],
-        ),
-        SQLite3Adapter.Column(
-            name="post_name",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("Post", "post_name")],
-        ),
-    ]
-
-    table = SQLite3Adapter.Table(name="Post", columns=columns)
-    
-
-class Comment(Table):  # type: ignore
-
-    columns = [
-        SQLite3Adapter.Column(
-            name="comment_id",
-            data_type="INTEGER",
-            constraints=[C.PrimaryKeyConstraint("Comment", "comment_id")],
-        ),
-        SQLite3Adapter.Column(
-            name="comment_name",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("Comment", "comment_name")],
-        ),
-    ]
-
-    table = SQLite3Adapter.Table(name="Comment", columns=columns)
+#     table = SQLite3Adapter.Table(name="Comment", columns=columns)
 
 
-### USUNIECIE I DODANIE TABELI
+# ### ZMIANA NAZWY
 
-class User(Table):  # type: ignore
-    columns = [
-        SQLite3Adapter.Column(
-            name="user_id",
-            data_type="INTEGER",
-            constraints=[C.PrimaryKeyConstraint("User", "user_id")],
-        ),
-        SQLite3Adapter.Column(
-            name="user_name",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("User", "user_name")],
-        ),
-    ]
+# class NewNamePost(Table):  # type: ignore
+#     columns = [
+#         SQLite3Adapter.Column(
+#             name="post_id",
+#             data_type="INTEGER",
+#             constraints=[C.PrimaryKeyConstraint("NewNamePost", "post_id")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="post_name",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("NewNamePost", "post_name")],
+#         ),
+#     ]
 
-    table = SQLite3Adapter.Table(name="User", columns=columns)
+#     table = SQLite3Adapter.Table(name="NewNamePost", columns=columns)r.Table(name="NewNamePost", columns=columns)
 
-class Post(Table):  # type: ignore
-    columns = [
-        SQLite3Adapter.Column(
-            name="post_id",
-            data_type="INTEGER",
-            constraints=[C.PrimaryKeyConstraint("Post", "post_id")],
-        ),
-        SQLite3Adapter.Column(
-            name="post_name",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("Post", "post_name")],
-        ),
-        SQLite3Adapter.Column(
-            name="post_content",
-            data_type="TEXT",
-            constraints=[C.NotNullConstraint("Post", "post_content")],
-        ),
-    ]
 
-    table = SQLite3Adapter.Table(name="Post", columns=columns)
+# ### Dodanie kolumny
 
+#         SQLite3Adapter.Column(
+#             name="post_content",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("Post", "post_content")],
+#         ),
+
+
+# ### POWROT
+
+# class User(Table):  # type: ignore
+#     columns = [
+#         SQLite3Adapter.Column(
+#             name="user_id",
+#             data_type="INTEGER",
+#             constraints=[C.PrimaryKeyConstraint("User", "user_id")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="user_name",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("User", "user_name")],
+#         ),
+#     ]
+
+#     table = SQLite3Adapter.Table(name="User", columns=columns)
+
+
+# class Post(Table):  # type: ignore
+#     columns = [
+#         SQLite3Adapter.Column(
+#             name="post_id",
+#             data_type="INTEGER",
+#             constraints=[C.PrimaryKeyConstraint("Post", "post_id")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="post_name",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("Post", "post_name")],
+#         ),
+#     ]
+
+#     table = SQLite3Adapter.Table(name="Post", columns=columns)
+
+
+# class Comment(Table):  # type: ignore
+
+#     columns = [
+#         SQLite3Adapter.Column(
+#             name="comment_id",
+#             data_type="INTEGER",
+#             constraints=[C.PrimaryKeyConstraint("Comment", "comment_id")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="comment_name",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("Comment", "comment_name")],
+#         ),
+#     ]
+
+#     table = SQLite3Adapter.Table(name="Comment", columns=columns)
+
+
+# ### USUNIECIE I DODANIE TABELI
+
+# class User(Table):  # type: ignore
+#     columns = [
+#         SQLite3Adapter.Column(
+#             name="user_id",
+#             data_type="INTEGER",
+#             constraints=[C.PrimaryKeyConstraint("User", "user_id")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="user_name",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("User", "user_name")],
+#         ),
+#     ]
+
+#     table = SQLite3Adapter.Table(name="User", columns=columns)
+
+# class Post(Table):  # type: ignore
+#     columns = [
+#         SQLite3Adapter.Column(
+#             name="post_id",
+#             data_type="INTEGER",
+#             constraints=[C.PrimaryKeyConstraint("Post", "post_id")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="post_name",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("Post", "post_name")],
+#         ),
+#         SQLite3Adapter.Column(
+#             name="post_content",
+#             data_type="TEXT",
+#             constraints=[C.NotNullConstraint("Post", "post_content")],
+#         ),
+#     ]
+
+#     table = SQLite3Adapter.Table(name="Post", columns=columns)
