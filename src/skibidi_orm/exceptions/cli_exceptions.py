40392c7bf5e846016d02aa71fd902836af93b289
xDepcio <olek.drwal@gmail.com>
2024-05-17 14:44:48 +0200

    added type command for starting studio server

---
+++ src/skibidi_orm/exceptions/cli_exceptions.py
@@ -0,0 +1,2 @@
+class MultipleSchemaFilesError(RuntimeError):
+    """Raised when multiple schema files are found using the GLOB_ALL_SCHMEAS pattern."""
--- src/skibidi_orm/migration_engine/studio/server.py
+++ src/skibidi_orm/migration_engine/studio/server.py
@@ -7,9 +7,9 @@ app = FastAPI()
 db_inspector: BaseDbInspector
 
 
-def run_server(schema_dir: str):
+def run_server(scheme_file: str):
     global db_inspector
-    db_inspector = get_db_inspector(schema_file=schema_dir)
+    db_inspector = get_db_inspector(schema_file=scheme_file)
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
