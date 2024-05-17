a416e1fedaf629980f0706a4b82e4dc745b46827
xDepcio <olek.drwal@gmail.com>
2024-05-17 20:27:28 +0200

    added test for SQLite3DBSeeder insert_row

---
+++ tests/skibidi-orm/migration_engine/db_seeders/test_sqlite3_seeder.py
@@ -0,0 +1,59 @@
+import pathlib
+
+import pytest
+
+from skibidi_orm.migration_engine.adapters.database_objects.constraints import (
+    NotNullConstraint,
+    PrimaryKeyConstraint,
+)
+from skibidi_orm.migration_engine.adapters.database_objects.sqlite3_typing import (
+    SQLite3Typing,
+)
+from skibidi_orm.migration_engine.db_config.sqlite3_config import SQLite3Config
+from skibidi_orm.migration_engine.db_inspectors.sqlite3_inspector import SqliteInspector
+from skibidi_orm.migration_engine.db_seeder.base_db_seeder import InsertRowColumn
+from skibidi_orm.migration_engine.db_seeder.sqlite3_db_seeder import SQLite3DBSeeder
+
+sql_simple_db = [
+    """
+    CREATE TABLE users (
+        user_id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL
+    );
+"""
+]
+
+
+@pytest.mark.parametrize("make_database", [[*sql_simple_db]], indirect=True)
+def test_insert_row(make_database: str):
+    SQLite3Config(db_path=make_database)
+    db_seeder = SQLite3DBSeeder()
+    db_seeder.insert_row(
+        table_name="users",
+        row=[
+            InsertRowColumn(name="user_id", value=str(1)),
+            InsertRowColumn(name="username", value="test"),
+        ],
+    )
+    db_inspector = SqliteInspector()
+    tables = db_inspector.get_tables()
+
+    assert tables[0] == SQLite3Typing.Table(
+        name="users",
+        columns=[
+            SQLite3Typing.Column(
+                name="user_id",
+                data_type="INTEGER",
+                constraints=[
+                    PrimaryKeyConstraint(table_name="users", column_name="user_id"),
+                ],
+            ),
+            SQLite3Typing.Column(
+                name="username",
+                data_type="TEXT",
+                constraints=[
+                    NotNullConstraint(table_name="users", column_name="username"),
+                ],
+            ),
+        ],
+    )
