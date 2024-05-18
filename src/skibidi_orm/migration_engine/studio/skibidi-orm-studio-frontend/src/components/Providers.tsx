5b18e1edb5b1a567077d6b4a42f16a9920fc5013
xDepcio <olek.drwal@gmail.com>
2024-05-18 02:35:01 +0200

    added dark mode provider

--- src/skibidi_orm/migration_engine/studio/skibidi-orm-studio-frontend/src/components/Providers.tsx
+++ src/skibidi_orm/migration_engine/studio/skibidi-orm-studio-frontend/src/components/Providers.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
+import { ThemeProvider } from './theme-provider'
 
 const queryClient = new QueryClient()
 
@@ -9,7 +10,9 @@ interface ProvidersProps {
 export const Providers: React.FC<ProvidersProps> = ({ children }) => {
     return (
         <QueryClientProvider client={queryClient}>
-            {children}
+            <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+                {children}
+            </ThemeProvider>
         </QueryClientProvider>
     )
 }
