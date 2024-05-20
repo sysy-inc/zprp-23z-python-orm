6f0bfc6e5bf7eabb598dcd1af95e6c0ad304da8b
xDepcio <olek.drwal@gmail.com>
2024-05-20 15:48:54 +0200

    basic component to display tables

--- src/skibidi_orm/migration_engine/studio/skibidi-orm-studio-frontend/src/lib/query-store.tsx
+++ src/skibidi_orm/migration_engine/studio/skibidi-orm-studio-frontend/src/lib/query-store.tsx
@@ -1,15 +1,34 @@
 import { useQuery } from '@tanstack/react-query'
 
+type QueryConstraint = {
+    constraint_type: string
+    table_name: string
+    column_name: string
+}
+
+type QueryColumn = {
+    name: string
+    data_type: string
+    constraints: QueryConstraint[]
+}
+
+type QueryTable = {
+    name: string
+    columns: QueryColumn[]
+}
+
 export const useQueryStore = () => {
-    const { data } = useQuery({
+    const tables = () => useQuery({
         queryKey: ['tablesData'],
         initialData: [],
         queryFn: async () => {
-            const response = await fetch('http://localhost:4000/tables')
+            const response = await fetch('http://localhost:8000/db')
             const data = await response.json()
-            return data
+            return data.tables as QueryTable[]
         },
     })
 
-    return data
+    return {
+        tables,
+    }
 }
