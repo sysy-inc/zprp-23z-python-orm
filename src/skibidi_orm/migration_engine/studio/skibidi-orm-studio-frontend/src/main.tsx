1e3b5320a47ff466b998dd45fb7d0bbe5b563e1c
xDepcio <olek.drwal@gmail.com>
2024-05-20 16:14:15 +0200

    tanstack router setup

--- src/skibidi_orm/migration_engine/studio/skibidi-orm-studio-frontend/src/main.tsx
+++ src/skibidi_orm/migration_engine/studio/skibidi-orm-studio-frontend/src/main.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Providers } from './components/Providers.tsx'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Providers>
-      <App />
+      <RouterProvider router={router} />
+      {/* <App /> */}
     </Providers>
   </React.StrictMode>,
 )
