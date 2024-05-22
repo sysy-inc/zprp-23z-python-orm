15a6b9743c307ad2744af31876565ab32d7fdf02
xDepcio <olek.drwal@gmail.com>
2024-05-22 22:21:14 +0200

    added nav sidebar

--- src/skibidi_orm/migration_engine/studio/skibidi-orm-studio-frontend/src/routes/__root.tsx
+++ src/skibidi_orm/migration_engine/studio/skibidi-orm-studio-frontend/src/routes/__root.tsx
@@ -1,10 +1,11 @@
-import { ModeToggle } from '@/components/mode-toggle'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { Sidebar } from '@/components/Sidebar'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
     component: () => (
         <div className='dark:bg-zinc-900 min-h-screen flex flex-col'>
-            <div className="p-2 flex gap-2  max-w-screen-xl mx-auto">
+            {/* <div className="p-2 flex gap-2  max-w-screen-xl mx-auto">
                 <Link to="/" className="[&.active]:font-bold">
                     Home
                 </Link>{' '}
@@ -12,10 +13,18 @@ export const Route = createRootRoute({
                     About
                 </Link>
                 <ModeToggle />
-            </div>
+            </div> */}
             {/* <hr /> */}
-            <Outlet />
+            <ResizablePanelGroup direction='horizontal' className='h-full flex-1'>
+                <ResizablePanel defaultSize={10}>
+                    <Sidebar />
+                </ResizablePanel>
+                <ResizableHandle />
+                <ResizablePanel>
+                    <Outlet />
+                </ResizablePanel>
+            </ResizablePanelGroup>
             {/* <TanStackRouterDevtools /> */}
-        </div>
+        </div >
     ),
 })
