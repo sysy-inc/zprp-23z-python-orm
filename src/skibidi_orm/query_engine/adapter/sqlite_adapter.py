a115714d4d819b6873e3091ed658e6265626da89
Marta Sobol <m.sob12@interia.pl>
2024-05-23 19:10:57 +0200

    add sql compiler to adapter

--- src/skibidi_orm/query_engine/adapter/sqlite_adapter.py
+++ src/skibidi_orm/query_engine/adapter/sqlite_adapter.py
@@ -3,12 +3,17 @@ Adapter for SQLite database
 """
 
 from skibidi_orm.query_engine.adapter.base_adapter import Adapter
+from skibidi_orm.query_engine.adapter.sqlite_compiler import SQLiteCompiler
 
 
 class SQLiteAdapter(Adapter):
     """
     Adapter for sqlite database
     """
+    def __init__(self) -> None:
+        super().__init__()
+        self._compiler = SQLiteCompiler()
+
     def _import_connector(self):
         """
         Import sqlite3, connector for SQLite databse
