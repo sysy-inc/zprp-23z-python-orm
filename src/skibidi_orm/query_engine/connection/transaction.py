6650e91343cc15b5aaa64e0bb20291238abb08c4
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2024-05-24 20:51:18 +0200

    add executing delete

--- src/skibidi_orm/query_engine/connection/transaction.py
+++ src/skibidi_orm/query_engine/connection/transaction.py
@@ -3,7 +3,7 @@ Transactions
 """
 from skibidi_orm.query_engine.adapter.base_compiler import SQLCompiler
 from skibidi_orm.query_engine.model.base import Model
-from skibidi_orm.query_engine.operations.crud import Insert, Update
+from skibidi_orm.query_engine.operations.crud import Insert, Update, Delete
 from typing import Any
 
 
@@ -13,6 +13,7 @@ class Transaction:
         self._conn = connection
         self._insert: list[Insert] = []
         self._update: list[Update] = []
+        self._delete: list[Delete] = []
 
     def register_insert(self, obj: Model):
         self._insert.append(Insert(obj))
@@ -20,11 +21,16 @@ class Transaction:
     def register_update(self, obj: Model):
         self._update.append(Update(obj))
 
+    def register_delete(self, obj: Model):
+        self._delete.append(Delete(obj))
+
     def execute(self):
         for insert in self._insert:
             self.execute_insert(insert)
         for update in self._update:
             self.execute_update(update)
+        for delete in self._delete:
+            self.execute_delete(delete)
 
     def execute_insert(self, statement: Insert):
         sql = self._compiler.insert(statement)
@@ -36,5 +42,10 @@ class Transaction:
 
     def execute_update(self, statement: Update):
         sql = self._compiler.update(statement)
+        cur = self._conn.cursor()
+        cur.execute(sql)
+
+    def execute_delete(self, statement: Delete):
+        sql = self._compiler.delete(statement)
         cur = self._conn.cursor()
         cur.execute(sql)
