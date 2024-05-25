047485118a8d024237242262750d0d9f12163eef
xDepcio <olek.drwal@gmail.com>
2024-05-25 15:08:43 +0200

    added data_mutator docstrings

--- src/skibidi_orm/migration_engine/data_mutator/base_data_mutator.py
+++ src/skibidi_orm/migration_engine/data_mutator/base_data_mutator.py
@@ -18,16 +18,20 @@ class BaseDataMutator(ABC):
 
     @abstractmethod
     def insert_row(self, table_name: str, row: list[InsertRowColumn]):
+        """Insert a row in the table."""
         pass
 
     @abstractmethod
     def delete_row(self, table_name: str, pks: list[DeleteRowPk]):
+        """Delete a row in the table. Row identified by primary key subset."""
         pass
 
     @abstractmethod
     def raw_query(self, query: str) -> list[Any]:
+        """Execute a raw sql query in the database."""
         pass
 
     @abstractmethod
     def get_rows(self, table_name: str, limit: int = 100, offset: int = 0) -> list[Any]:
+        """Get paginated rows from the table."""
         pass
--- src/skibidi_orm/migration_engine/data_mutator/sqlite3_data_mutatorr.py
+++ src/skibidi_orm/migration_engine/data_mutator/sqlite3_data_mutatorr.py
@@ -14,11 +14,13 @@ class SQLite3DataMutator(BaseDataMutator):
         self.config = SQLite3Config.get_instance()
 
     def insert_row(self, table_name: str, row: list[InsertRowColumn]):
+        """Insert a row in the table."""
         query = f"INSERT INTO {table_name} ({', '.join([col.name for col in row])}) VALUES ({', '.join(['?' for _ in row])})"
         values = [col.value for col in row]
         self._sqlite_execute(query, values)
 
     def delete_row(self, table_name: str, pks: list[DeleteRowPk]):
+        """Delete a row in the table. Row identified by primary key subset."""
         deleted_rows = self._pre_select_to_delete_rows(table_name, pks)
         if len(deleted_rows) > 1:
             raise AmbigiousDeleteRowError(
@@ -29,6 +31,7 @@ class SQLite3DataMutator(BaseDataMutator):
         self._sqlite_execute(query, values)
 
     def _pre_select_to_delete_rows(self, table_name: str, pks: list[DeleteRowPk]):
+        """Select rows to be deleted. Used to check if the deletion is ambigious."""
         query = f"SELECT * FROM {table_name} WHERE {' AND '.join([f'{pk.name} = ?' for pk in pks])}"
         values = [pk.value for pk in pks]
         return self._sqlite_execute(query, values)
@@ -48,8 +51,10 @@ class SQLite3DataMutator(BaseDataMutator):
         return data
 
     def raw_query(self, query: str):
+        """Execute a raw sql query in the database."""
         return self._sqlite_execute(query, [])
 
     def get_rows(self, table_name: str, limit: int = 100, offset: int = 0):
+        """Get paginated rows from the table."""
         query = f"SELECT * FROM {table_name} LIMIT {limit} OFFSET {offset}"
         return self._sqlite_execute(query, [])
