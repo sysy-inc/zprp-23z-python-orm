caf6f56cc15e8dae5016d6e65e5646f01d52d6ba
jedrzej-grabski <jedrzej@grabski.pl>
2024-05-28 22:24:40 +0200

    Typo fix

--- src/skibidi_orm/migration_engine/adapters/sqlite3_typing.py
+++ src/skibidi_orm/migration_engine/adapters/sqlite3_typing.py
@@ -1,11 +1,12 @@
 from typing import Literal
 from skibidi_orm.migration_engine.adapters.base_adapter import (
     BaseColumn,
-    Relation,
     BaseTable,
 )
 
-from skibidi_orm.migration_engine.operations.constraints import Constraint
+from skibidi_orm.migration_engine.adapters.database_objects.constraints import (
+    Constraint,
+)
 
 
 class SQLite3Typing:
@@ -14,6 +15,4 @@ class SQLite3Typing:
     Constraints = Constraint
     Column = BaseColumn[DataTypes]
     Table = BaseTable[Column]
-    Relation = Relation
     tables: list[Table] = []
-    relations: list[Relation] = []
