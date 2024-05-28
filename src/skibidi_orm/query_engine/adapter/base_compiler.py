4da291f358359245e54e0fc7c1c1bcdadada06e3
Marta Sobol <m.sob12@interia.pl>
2024-05-28 22:20:57 +0200

    add sql for select

--- src/skibidi_orm/query_engine/adapter/base_compiler.py
+++ src/skibidi_orm/query_engine/adapter/base_compiler.py
@@ -3,12 +3,25 @@ Base sql compiler
 """
 from skibidi_orm.query_engine.operations.crud import Insert, ValueBase, Update, Delete
 from skibidi_orm.query_engine.operations import clauses as c
+from skibidi_orm.query_engine.operations.select import Select
+from skibidi_orm.query_engine.operations.functions import Function, Count
 from typing import Any, Type
 
 
 class SQLCompiler:
     CLAUSES: dict[Type[c.Clause], str] = {
-        c.Eq: "="
+        c.Eq: "=",
+        c.Gt: ">",
+        c.GtEq: ">=",
+        c.Lt: "<",
+        c.LtEq: "<=",
+        c.NotEq: "!=",
+        c.NotNull: " IS NOT NULL",
+        c.Null: " IS NULL"
+    }
+
+    FUNCTIONS: dict[Type[Function], str] = {
+        Count: "COUNT"
     }
 
     def _prepare_columns(self, statement: ValueBase) -> str:
@@ -17,7 +30,10 @@ class SQLCompiler:
         return text
 
     def _prep_val(self, val: Any):
-        return f"'{val}'" if isinstance(val, str) else str(val)
+        text = ""
+        if val:
+            text = f"'{val}'" if isinstance(val, str) else str(val)
+        return text
 
     def _prepare_values(self, statement: ValueBase) -> str:
         val = statement.values()
@@ -56,11 +72,12 @@ class SQLCompiler:
             text += f'{col}={self._prep_val(val)}, '
         return text[:-2]
 
-    def _prepare_where(self, clauses: tuple[c.Clause]):
+    def _prepare_where(self, clauses: tuple[c.Clause, ...]):
         text = "WHERE "
         for clause in clauses:
-            text += f"{clause.col}{self.CLAUSES.get(clause.type)}{self._prep_val(clause.val)}, "
-        return text[:-2]
+            text += f"{clause.col}{self.CLAUSES.get(clause.type)}{self._prep_val(clause.val)}"
+            text += " AND "
+        return text[:-5]
 
     def update(self, statement: Update) -> str:
         text = "UPDATE "
@@ -78,6 +95,58 @@ class SQLCompiler:
         text += statement.table()
         text += " "
         text += self._prepare_where(statement.where_clause())
+        text += ";"
+        print(text)
+        return text
+    
+    def _agregate_function(self, func: Function):
+        text = f"{self.FUNCTIONS[func.type]}({func.column})"
+        return text
+    
+    def _alias(self, alias: str):
+        text = f" AS {alias}"
+        return text
+    
+    def _select_columns(self, statement: Select):
+        fields = statement.fields
+        text = ""
+        for field in fields:
+            # check if agregate function
+            if isinstance(field, Function):
+                text += self._agregate_function(field)
+            # just column name
+            else:
+                text += field
+
+            if field in statement.annotations:
+                # there is an alias to be made
+                text += self._alias(statement.annotations[field])
+
+            text += ", "
+        return text[:-2]
+    
+    def select(self, statement: Select) -> str:
+        text = "SELECT "
+        text += self._select_columns(statement)
+        text += " "
+        text += f"FROM {statement.table} "
+        # WHERE
+        if statement.where_clauses:
+            text += self._prepare_where(tuple(statement.where_clauses))
+            text += " "
+        # GROUP BY
+        if statement.group_by_col:
+            text += "GROUP BY "
+            text += ", ".join(statement.group_by_col)
+            text += " "
+        # ORDERE BY
+        if statement.order_by_col[0]:
+            text += "ORDER BY "
+            text += ", ".join(statement.order_by_col[0])
+            if statement.order_by_col[1]:
+                # it is descend
+                text += " DESC"
+            text += " "
         text += ";"
         print(text)
         return text
