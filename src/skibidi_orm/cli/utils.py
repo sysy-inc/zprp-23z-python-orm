4b7daf12e3d061173b422cec6330179e27fee4da
xDepcio <olek.drwal@gmail.com>
2024-05-29 01:16:54 +0200

    fixed isssues in conversations: typo in GLOB_ALL_SCHEMAS Missing docstring for find_schema_file function

--- src/skibidi_orm/cli/utils.py
+++ src/skibidi_orm/cli/utils.py
@@ -1,11 +1,16 @@
 import glob
 from skibidi_orm.exceptions.cli_exceptions import MultipleSchemaFilesError
 
-GLOB_ALL_SCHMEAS = "**/**/schema.py"
+GLOB_ALL_SCHEMAS = "**/**/schema.py"
 
 
 def find_schema_file():
-    files = set(glob.glob(GLOB_ALL_SCHMEAS, recursive=True))
+    """
+    Find the schema file in the project.
+    If no schema file is found, raise FileNotFoundError.
+    If multiple schema files are found, raise MultipleSchemaFilesError.
+    """
+    files = set(glob.glob(GLOB_ALL_SCHEMAS, recursive=True))
     if len(files) == 0:
         raise FileNotFoundError("No schema file found.")
     if len(files) > 1:
