6747a69500f45ba174da47a7d0715f1f24702dfe
xDepcio <olek.drwal@gmail.com>
2024-05-29 18:15:01 +0200

    added tests for PostgresInspector get_tables_names method

---
+++ tests/skibidi-orm/migration_engine/db_inspector/test_postgres_inspector.py
@@ -0,0 +1,91 @@
+import pytest
+from skibidi_orm.migration_engine.db_config.postgres_config import PostgresConfig
+from skibidi_orm.migration_engine.db_inspectors.postgres_inspector import (
+    PostgresInspector,
+)
+from ..conftest import postgres_db_fixture
+
+sql_table1 = """
+    CREATE TABLE table1 (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+    );
+"""
+sql_table2 = """
+    CREATE TABLE table2 (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+    );
+"""
+sql_table_primary_key_not_null = """
+    CREATE TABLE table_primary_key_not_null (
+        id INTEGER PRIMARY KEY NOT NULL
+    );
+"""
+
+sql_schema_with_fks = [
+    """
+    CREATE TABLE users (
+        user_id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL,
+        registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+""",
+    """
+    CREATE TABLE posts (
+        post_id INTEGER PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL,
+        post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    );
+""",
+    """
+    CREATE TABLE comments (
+        comment_id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        user_idd INTEGER NOT NULL,
+        post_id INTEGER NOT NULL,
+        comment_text TEXT NOT NULL,
+        comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_idd, username) REFERENCES users(user_id, username),
+        FOREIGN KEY (post_id) REFERENCES posts(post_id)
+    );
+""",
+]
+
+
+@pytest.mark.parametrize(
+    "query, expected_tables_len, expected_tables_list",
+    [
+        ([sql_table1], 1, ["table1"]),
+        ([sql_table1, sql_table2], 2, ["table1", "table2"]),
+        ([sql_table_primary_key_not_null], 1, ["table_primary_key_not_null"]),
+    ],
+)
+def test_get_tables_names(query, expected_tables_list, expected_tables_len):  # type: ignore
+    @postgres_db_fixture(
+        db_name="postgres",
+        db_user="admin",
+        db_password="admin",
+        db_host="0.0.0.0",
+        db_port=5432,
+        queries=query,  # type: ignore
+    )
+    def test_fn():
+        PostgresConfig(
+            db_name="postgres",
+            db_user="admin",
+            db_password="admin",
+            db_host="0.0.0.0",
+            db_port=5432,
+        )
+        inspector = PostgresInspector()
+        tables = inspector.get_tables_names()
+        assert len(tables) == expected_tables_len
+        assert sorted(expected_tables_list) == sorted(tables)  # type: ignore
+
+    test_fn()
