f640de1f69b8ae87ab8b99a27a31a0b0d76ee792
jedrzej-grabski <grabski.dev@gmail.com>
2024-05-29 15:10:33 +0200

    Added docstring to the Typing class

--- src/skibidi_orm/migration_engine/adapters/sqlite3_typing.py
+++ src/skibidi_orm/migration_engine/adapters/sqlite3_typing.py
@@ -10,6 +10,9 @@ from skibidi_orm.migration_engine.adapters.database_objects.constraints import (
 
 
 class SQLite3Typing:
+    """
+    Class defining the typing for SQLite3 database objects. It has to be an abstract class to avoid circular imports.
+    """
 
     DataTypes = Literal["TEXT", "INTEGER", "REAL", "BLOB", "NULL"]
     Constraints = Constraint
