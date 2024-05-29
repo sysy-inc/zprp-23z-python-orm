28d05d6040fa4e7725e87b7e946869e9fe3b9dc2
xDepcio <olek.drwal@gmail.com>
2024-05-30 00:20:54 +0200

    added tests for _get_column_data_types method on PostgresInspector

--- tests/skibidi-orm/migration_engine/db_inspector/test_postgres_inspector.py
+++ tests/skibidi-orm/migration_engine/db_inspector/test_postgres_inspector.py
@@ -81,3 +81,158 @@ def test__get_table_columns_names():
         )
 
     test_fn()
+
+
+@pytest.mark.parametrize(
+    "query, table_name, column_name, expected_data_type",
+    [
+        (  # 0
+            [PostgresTablesData.SQL_TABLE_MANY_COLUMNS1],
+            "table_many_columns1",
+            "primary_key",
+            "INTEGER",
+        ),
+        (  # 1
+            [PostgresTablesData.SQL_TABLE_MANY_COLUMNS1],
+            "table_many_columns1",
+            "text_not_null",
+            "TEXT",
+        ),
+        (  # 2
+            [PostgresTablesData.SQL_TABLE_MANY_COLUMNS1],
+            "table_many_columns1",
+            "text_nullabe",
+            "TEXT",
+        ),
+        (  # 3
+            [PostgresTablesData.SQL_TABLE_MANY_COLUMNS1],
+            "table_many_columns1",
+            "integer_not_null",
+            "INTEGER",
+        ),
+        (  # 4
+            [PostgresTablesData.SQL_TABLE_MANY_COLUMNS1],
+            "table_many_columns1",
+            "integer_nullable",
+            "INTEGER",
+        ),
+        (  # 5
+            [PostgresTablesData.SQL_TABLE_MANY_COLUMNS1],
+            "table_many_columns1",
+            "date_not_null",
+            "DATE",
+        ),
+        (  # 6
+            [PostgresTablesData.SQL_TABLE_MANY_COLUMNS1],
+            "table_many_columns1",
+            "date_nullable",
+            "DATE",
+        ),
+        (  # 7
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "big_primary_key_alias",
+            "BIGINT",
+        ),
+        (  # 8
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "bit_varying_alias",
+            "BIT VARYING",
+        ),
+        (  # 9
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "bit_varying_alias_arg",
+            "BIT VARYING",
+        ),
+        (  # 10
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "double_precision",
+            "DOUBLE PRECISION",
+        ),
+        (  # 11
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "timestamp",
+            "TIMESTAMP WITHOUT TIME ZONE",
+        ),
+        (  # 12
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "timestamp_arg",
+            "TIMESTAMP WITHOUT TIME ZONE",
+        ),
+        (  # 13
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "timestamp_without_time_zone",
+            "TIMESTAMP WITHOUT TIME ZONE",
+        ),
+        (  # 14
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "timestamp_without_time_zone_arg",
+            "TIMESTAMP WITHOUT TIME ZONE",
+        ),
+        (  # 15
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "timestamp_with_time_zone_alias",
+            "TIMESTAMP WITH TIME ZONE",
+        ),
+        (  # 16
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "timestamp_with_time_zone_arg_alias",
+            "TIMESTAMP WITH TIME ZONE",
+        ),
+        (  # 17
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "charachter_varying",
+            "CHARACTER VARYING",
+        ),
+        (  # 18
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "charachter_varying_arg",
+            "CHARACTER VARYING",
+        ),
+        (  # 19
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "charachter_varying_alias",
+            "CHARACTER VARYING",
+        ),
+        (  # 20
+            [PostgresTablesData.SQL_TABLE_DIFFICULT_TYPES],
+            "table_difficult_types",
+            "charachter_varying_alias_args",
+            "CHARACTER VARYING",
+        ),
+    ],
+)
+def test__get_column_data_type(query, table_name, column_name, expected_data_type):  # type: ignore
+    @postgres_db_fixture(
+        db_name="postgres",
+        db_user="admin",
+        db_password="admin",
+        db_host="0.0.0.0",
+        db_port=5432,
+        queries=query,  # type: ignore
+    )
+    def test_fn():
+        PostgresConfig(
+            db_name="postgres",
+            db_user="admin",
+            db_password="admin",
+            db_host="0.0.0.0",
+            db_port=5432,
+        )
+        inspector = PostgresInspector()
+        data_type = inspector._get_column_data_type(table_name, column_name)  # type: ignore
+        assert data_type == expected_data_type
+
+    test_fn()
