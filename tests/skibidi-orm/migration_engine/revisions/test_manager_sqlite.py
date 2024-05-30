a8bc029759257c275d2f261d32c03b049e52289c
maksym <01178511@pw.edu.pl>
2024-05-30 20:08:52 +0200

    refactor revision manager tests

--- tests/skibidi-orm/migration_engine/revisions/test_manager_sqlite.py
+++ tests/skibidi-orm/migration_engine/revisions/test_manager_sqlite.py
@@ -15,7 +15,7 @@ from skibidi_orm.migration_engine.sql_executor.sqlite3_executor import SQLite3Ex
 import pytest
 
 sql_schema_with_fks = [
-    # todo: remove after merging main
+    # todo: remove after postgres is merged
     """
     CREATE TABLE users (
         user_id INTEGER PRIMARY KEY,
@@ -49,6 +49,40 @@ sql_schema_with_fks = [
 """,
 ]
 
+simple_testing_schema = [
+    "CREATE TABLE test_table (id INTEGER, name TEXT);",
+    "CREATE TABLE test_table_2 (idd INTEGER, count_ INTEGER, FOREIGN KEY (idd) REFERENCES test_table (id));",
+    "INSERT INTO test_table (id, name) VALUES (1, 'test');",
+]
+
+table_1 = SQLite3Typing.Table(
+    "test_table",
+    [SQLite3Typing.Column("id", "INTEGER"), SQLite3Typing.Column("name", "TEXT")],
+)
+
+table_2 = SQLite3Typing.Table(
+    "test_table_2",
+    [
+        SQLite3Typing.Column("idd", "INTEGER"),
+        SQLite3Typing.Column("count_", "INTEGER"),
+    ],
+    {ForeignKeyConstraint("test_table_2", "test_table", {"idd": "id"})},
+)
+
+revision_1 = Revision(
+    "test description",
+    "test schema repr",
+    DatabaseProvider.SQLITE3,
+    [table_1, table_2],
+)
+
+revision_2 = Revision(
+    "test description_2",
+    "test schema repr_2",
+    DatabaseProvider.SQLITE3,
+    [SQLite3Typing.Table("test_table", [])],
+)
+
 
 def test_create_and_find_revision_table_sqlite(
     make_database: str, monkeypatch: pytest.MonkeyPatch
@@ -56,6 +90,7 @@ def test_create_and_find_revision_table_sqlite(
     """Tests whether the revision table is found if it does not exist"""
     monkeypatch.setattr(RevisionManager, "create_revision_table", lambda _: None)  # type: ignore
     monkeypatch.setattr(RevisionManager, "get_all_revisions", lambda _: [])  # type: ignore
+
     SQLite3Config(make_database)
     manager = RevisionManager()
     assert not manager.find_revision_table()
@@ -66,15 +101,10 @@ def test_save_revision_sqlite(make_database: str):
     any database errors"""
     SQLite3Config(make_database)
     manager = RevisionManager()
+
     if not manager.find_revision_table():
         manager.create_revision_table()
-    revision = Revision(
-        "test description",
-        "test schema repr",
-        DatabaseProvider.SQLITE3,
-        [],
-    )
-    manager.save_revision(revision)
+    manager.save_revision(revision_1)
 
 
 def test_save_and_get_revision_sqlite(make_database: str):
@@ -82,96 +112,41 @@ def test_save_and_get_revision_sqlite(make_database: str):
     from the database"""
     SQLite3Config(make_database)
     manager = RevisionManager()
+
     if not manager.find_revision_table():
         manager.create_revision_table()
-    revision = Revision(
-        "test description",
-        "test schema repr",
-        DatabaseProvider.SQLITE3,
-        [],
-    )
-    revision_2 = Revision(
-        "test description_2",
-        "test schema repr_2",
-        DatabaseProvider.SQLITE3,
-        [SQLite3Typing.Table("test_table", [])],
-    )
-    manager.save_revision(revision)
+
+    manager.save_revision(revision_1)
     manager.save_revision(revision_2)
+
     revisions = manager.get_all_revisions()
+
     assert len(revisions) == 2
-    assert revisions[1] == revision
+    assert revisions[1] == revision_1
     assert revisions[2] == revision_2
 
 
+@pytest.mark.parametrize("make_database", [simple_testing_schema], indirect=True)
 def test_clear_database_sqlite(make_database: str):
     """Clearing the database removes everything but the revisions table and its contents"""
     SQLite3Config(make_database)
-    table_1 = SQLite3Typing.Table(
-        "test_table",
-        [SQLite3Typing.Column("id", "INTEGER"), SQLite3Typing.Column("name", "TEXT")],
-    )
-
-    table_2 = SQLite3Typing.Table(
-        "test_table_2",
-        [
-            SQLite3Typing.Column("idd", "INTEGER"),
-            SQLite3Typing.Column("count_", "INTEGER"),
-        ],
-        {ForeignKeyConstraint("test_table_2", "test_table", {"idd": "id"})},
-    )
-    SQLite3Executor.execute_sql(
-        """
-        CREATE TABLE test_table (id INTEGER, name TEXT);
-        CREATE TABLE test_table_2 (idd INTEGER, count_ INTEGER, FOREIGN KEY (idd) REFERENCES test_table (id));
-        INSERT INTO test_table (id, name) VALUES (1, 'test');
-        """
-    )
     manager = RevisionManager()
-    revision = Revision(
-        "test description",
-        "test schema repr",
-        DatabaseProvider.SQLITE3,
-        [table_1, table_2],
-    )
-    manager.save_revision(revision)
+
+    manager.save_revision(revision_1)
     manager.clear_database()
+
     revisions = manager.get_all_revisions()
     assert len(revisions) == 1
     revision_ = revisions.values().__iter__().__next__()
-    assert revision_ == revision
+    assert revision_ == revision_1
 
 
+@pytest.mark.parametrize("make_database", [simple_testing_schema], indirect=True)
 def test_get_revision_SQL_sqlite(make_database: str):
     SQLite3Config(make_database)
-    table_1 = SQLite3Typing.Table(
-        "test_table",
-        [SQLite3Typing.Column("id", "INTEGER"), SQLite3Typing.Column("name", "TEXT")],
-    )
-
-    table_2 = SQLite3Typing.Table(
-        "test_table_2",
-        [
-            SQLite3Typing.Column("idd", "INTEGER"),
-            SQLite3Typing.Column("count_", "INTEGER"),
-        ],
-        {ForeignKeyConstraint("test_table_2", "test_table", {"idd": "id"})},
-    )
-    SQLite3Executor.execute_sql(
-        """
-        CREATE TABLE test_table (id INTEGER, name TEXT);
-        CREATE TABLE test_table_2 (idd INTEGER, count_ INTEGER, FOREIGN KEY (idd) REFERENCES test_table (id));
-        INSERT INTO test_table (id, name) VALUES (1, 'test');
-        """
-    )
     manager = RevisionManager()
-    revision = Revision(
-        "test description",
-        "test schema repr",
-        DatabaseProvider.SQLITE3,
-        [table_1, table_2],
-    )
-    revision_sql = manager.get_revision_SQL(revision)
+
+    revision_sql = manager.get_revision_SQL(revision_1)
     assert revision_sql == (
         "CREATE TABLE test_table (id INTEGER, name TEXT);\n"
         "CREATE TABLE test_table_2 (idd INTEGER, count_ INTEGER, FOREIGN KEY (idd) "
@@ -183,32 +158,12 @@ def test_go_to_revision_from_empty_db_sqlite(make_database: str):
     """Tests whether the go_to_revision method works when the database is empty"""
     SQLite3Config(make_database)
 
-    # todo: parametrize via pytest
-    table_1 = SQLite3Typing.Table(
-        "test_table",
-        [SQLite3Typing.Column("id", "INTEGER"), SQLite3Typing.Column("name", "TEXT")],
-    )
-
-    table_2 = SQLite3Typing.Table(
-        "test_table_2",
-        [
-            SQLite3Typing.Column("idd", "INTEGER"),
-            SQLite3Typing.Column("count_", "INTEGER"),
-        ],
-        {ForeignKeyConstraint("test_table_2", "test_table", {"idd": "id"})},
-    )
-
-    revision = Revision(
-        "test description",
-        "test schema repr",
-        DatabaseProvider.SQLITE3,
-        [table_1, table_2],
-    )
     manager = RevisionManager()
-    manager.save_revision(revision)
+    manager.save_revision(revision_1)
     revision_id = manager.get_all_revisions().keys().__iter__().__next__()
-    assert manager.get_revision_by_id(revision_id) == revision
-    manager.go_to_revision(revision)
+
+    assert manager.get_revision_by_id(revision_id) == revision_1
+    manager.go_to_revision(revision_1)
 
     all_tables = SQLite3Inspector().get_tables()
 
@@ -222,28 +177,9 @@ def test_go_to_revision_from_non_empty_db_sqlite(make_database: str):
     SQLite3Config(make_database)
     inspector = SQLite3Inspector()
     assert len(inspector.get_tables()) == 3
-    table_1 = SQLite3Typing.Table(
-        "test_table",
-        [SQLite3Typing.Column("id", "INTEGER"), SQLite3Typing.Column("name", "TEXT")],
-    )
 
-    table_2 = SQLite3Typing.Table(
-        "test_table_2",
-        [
-            SQLite3Typing.Column("idd", "INTEGER"),
-            SQLite3Typing.Column("count_", "INTEGER"),
-        ],
-        {ForeignKeyConstraint("test_table_2", "test_table", {"idd": "id"})},
-    )
-
-    revision = Revision(
-        "test description",
-        "test schema repr",
-        DatabaseProvider.SQLITE3,
-        [table_1, table_2],
-    )
     manager = RevisionManager()
-    manager.go_to_revision(revision)
+    manager.go_to_revision(revision_1)
     new_tables = inspector.get_tables()
     assert table_1 in new_tables
     assert table_2 in new_tables
