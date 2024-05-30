182f0163b30effbf03d246a74d5dec93b3891fc0
Maria Polakowska <01169227@pw.edu.pl>
2024-05-30 18:53:41 +0200

    add docstrig

--- src/skibidi_orm/query_engine/model/base.py
+++ src/skibidi_orm/query_engine/model/base.py
@@ -1,3 +1,7 @@
+"""
+Module handles managing database model instances with attributes and relationships.
+"""
+
 from pydantic import BaseModel, Field
 from pydantic._internal._model_construction import ModelMetaclass
 from typing import Any
@@ -8,13 +12,44 @@ from skibidi_orm.query_engine.connection.session import Session
 
 
 def _is_field(value: Any):
-    # Only call contribute_to_class() if it's bound.
+    """Check if a value is a field in a Django model.
+
+    This function determines if a given value is a field in a Django model by
+    checking if it has the `contribute_to_class` attribute and is not a class.
+
+    Args:
+        value (Any): The value to be checked.
+
+    Returns:
+        bool: True if the value is a field, False otherwise.
+    """
     return not inspect.isclass(value) and hasattr(value, "contribute_to_class")
 
 
 class MetaModel(ModelMetaclass):
-    """ A metaclass for model """
+    """A metaclass for model.
+
+    This metaclass is responsible for setting up the metadata and attributes
+    for Django-like models. It handles the initialization of fields, table
+    names, and other metadata.
+    """
+
     def __new__(cls, name: str, bases: tuple[Any], attrs: dict[str, Any]) -> Any:
+        """Create a new instance of the model class.
+
+        This method sets up the metadata and fields for the model class. It ensures
+        that the initialization is only performed for subclasses of Model, excluding
+        the Model class itself.
+
+        Args:
+            cls (type): The metaclass.
+            name (str): The name of the new class.
+            bases (tuple[Any]): The base classes of the new class.
+            attrs (dict[str, Any]): The attributes of the new class.
+
+        Returns:
+            Any: The new class.
+        """
         super_new: Any = super().__new__ # type: ignore
 
         # Ensure initialization is only performed for subclasses of Model
@@ -45,18 +80,44 @@ class MetaModel(ModelMetaclass):
         new_class._meta._prepare(new_class)
         return new_class
 
-    def add_to_class(cls, obj_name: str, obj: Any): # type: ignore
-        """ Adds attribute to class """
+    def add_to_class(cls, obj_name: str, obj: Any):
+        """Add an attribute to the class.
+
+        This method adds a specified attribute to the class.
+
+        Args:
+            cls (type): The class to add the attribute to.
+            obj_name (str): The name of the attribute.
+            obj (Any): The attribute to add.
+        """
         obj.contribute_to_class(cls, obj_name)
 
 
 class Model(BaseModel, metaclass=MetaModel):
-    """ A class to create your own database table """
+    """A metaclass to create your own database table.
+
+    This class represents a database model, handling initialization of fields,
+    setting primary keys, and managing relationships and attributes.
+
+    """
     class Config:
         arbitrary_types_allowed = True
         allow_population_by_field_name = True
 
     def __init__(self, *args: Any, **kwargs: Any):
+        """Initialize a new instance of the model.
+
+        This method sets up the fields and attributes for the model instance,
+        ensuring that the correct values are assigned and managing relationships.
+
+        Args:
+            *args (Any): Positional arguments for field values.
+            **kwargs (Any): Keyword arguments for field values.
+
+        Raises:
+            IndexError: If the number of positional arguments exceeds the number
+                        of fields.
+        """
         opts: MetaOptions = self._meta
         if len(args) > len(opts.local_fields):
             raise IndexError("Number of args exceeds number of fields")
@@ -65,7 +126,7 @@ class Model(BaseModel, metaclass=MetaModel):
         for val, field in zip(args, fields_iter):
             kwargs[field.name] = val
         for field in fields_iter:
-            if field.is_relation and field.db_column in kwargs:
+            if field.is_relation and field.column in kwargs:
                 kwargs[field.name] = None   # add none to have attr value
             elif field.name not in kwargs:
                 kwargs[field.name] = field.default
@@ -78,21 +139,39 @@ class Model(BaseModel, metaclass=MetaModel):
 
         # if foreign key is given by id set it correct
         for field in self._meta.relation_fields:
-            value = kwargs.get(field.db_column, None)
-            object.__setattr__(self, field.db_column, value)
+            value = kwargs.get(field.column, None)
+            object.__setattr__(self, field.column, value)
         self._changes: dict [str, str] = {}
 
 
-    def _get_pk_val(self):
+    def _get_pk_val(self) -> Any:
+        """Get the value of the primary key.
+
+        Returns:
+            Any: The value of the primary key.
+        """
         return getattr(self, self._meta.primary_key.name)
 
-    def _set_pk_val(self, value: Any):
+    def _set_pk_val(self, value: Any) -> None:
+        """Set the value of the primary key.
+
+        Args:
+            value (Any): The value to set as the primary key.
+        """
         name = self._meta.primary_key.name
         setattr(self, name, value)
 
     pk = property(_get_pk_val, _set_pk_val)
 
     def __eq__(self, other: object):
+        """Check equality between two model instances.
+
+        Args:
+            other (object): The object to compare with.
+
+        Returns:
+            bool: True if the objects are equal, False otherwise.
+        """
         if not isinstance(other, Model):
             return NotImplemented
         if type(self) != type(other):
@@ -103,25 +182,39 @@ class Model(BaseModel, metaclass=MetaModel):
         return my_pk == other.pk
 
     def __setattr__(self, name: str, value: Any) -> None:
-        if name not in self.__fields__:
-            self.__fields__[name] = Field(default=None)
+        """Set an attribute value.
+
+        Args:
+            name (str): The name of the attribute.
+            value (Any): The value to set.
+        """
+        if name not in self.__fields__: # type: ignore
+            self.__fields__[name] = Field(default=None) # type: ignore
         super().__setattr__(name, value)
-        if hasattr(self, '_changes') and name != '_changes':
+        if hasattr(self, '_changes') and name != '_changes' and name != '_session':
             self._changes[name] = value
             if hasattr(self, '_session'):
-                self._session.changed(self)
+                self._session.changed(self) # type: ignore
         if name in self._meta.relation_fields_name():
             field = self._meta.get_relation_field(name)
-            object.__setattr__(self, field.db_column, None)
+            object.__setattr__(self, field.column, None)
         elif name in self._meta.relation_fields_column():
             field = self._meta.get_relation_field(name)
             object.__setattr__(self, field.name, None)
 
     def __getattribute__(self, name: str) -> Any:
+        """Get an attribute value.
+
+        Args:
+            name (str): The name of the attribute.
+
+        Returns:
+            Any: The value of the attribute.
+        """
         value = super().__getattribute__(name)
         if value is None and name in self._meta.relation_fields_name():
             field = self._meta.get_relation_field(name)
-            if object.__getattribute__(self, field.db_column):
+            if object.__getattribute__(self, field.column):
                 # if id select obj from db
                 # TODO add select if is id
                 value = 5
@@ -133,32 +226,63 @@ class Model(BaseModel, metaclass=MetaModel):
         return value
 
     def _get_name_and_pk(self):
-        return (self._meta.db_table, self.pk)
+        """Get the table name and primary key value.
 
-    def _get_attr_values(self):
+        Returns:
+            tuple: The table name and primary key value.
+        """
+        return self._meta.db_table, self.pk
+
+    def _get_attr_values(self) -> list[Any]:
+        """Get a list of attribute values.
+
+        Returns:
+            list: A list of attribute values.
+        """
         atrr_values : list[Any] = []
         for field in self._meta.local_fields:
             if field.is_relation:
-                atrr_values.append((field.db_column, getattr(self, field.db_column)))
+                atrr_values.append((field.column, getattr(self, field.column)))
             else:
                 value = getattr(self, field.name)
                 if value is not None:
                     atrr_values.append((field.name, value))
         return atrr_values
 
-    def _update_changes_db(self):
+    def _update_changes_db(self) -> dict[str, str]:
+        """Update changes in the database.
+
+        Returns:
+            dict: A dictionary of changes.
+        """
         changes = self._changes
         self._changes = {}
         return changes
 
-    def _add_session(self, session: Session):
+    def _add_session(self, session: Session) -> None:
+        """Add a session to the instance.
+
+        Args:
+            session (Session): The session to add.
+        """
         self._session = session
 
-    def _remove_session(self):
+    def _remove_session(self) -> None:
+        """Remove the session from the instance."""
         self._session = None
 
-    def _is_pk_none(self):
+    def _is_pk_none(self) -> bool:
+        """Check if the primary key is None.
+
+        Returns:
+            bool: True if the primary key is None, False otherwise.
+        """
         return self.pk is None and isinstance(self._meta.primary_key, AutoField)
 
     def _get_db_pk(self):
-        return self._meta.primary_key.db_column, self.pk
+        """Get the database primary key value.
+
+        Returns:
+            tuple: The primary key column name and value.
+        """
+        return self._meta.primary_key.column, self.pk
--- src/skibidi_orm/query_engine/model/meta_options.py
+++ src/skibidi_orm/query_engine/model/meta_options.py
@@ -1,3 +1,7 @@
+"""
+Module handles managing additional information for the database model.
+"""
+
 from typing import Any, Dict, List
 from pydantic import Field
 import bisect
@@ -5,8 +9,18 @@ from skibidi_orm.query_engine.field.field import AutoField
 
 
 class MetaOptions:
-    """ A subsidiary to store additional information"""
+    """A subsidiary to store additional information.
+
+    This class stores meta information related to a database model, including
+    table name, primary key, local fields, and relation fields.
+    """
+
     def __init__(self, meta: Dict[str, Any]):
+        """Initializes a MetaOptions instance.
+
+        Args:
+            meta (Dict[str, Any]): A dictionary containing meta information.
+        """
         self.db_table = ''
         self.meta = meta
         self.primary_key = None
@@ -14,15 +28,37 @@ class MetaOptions:
         self.relation_fields: List[Any] = []
 
     def contribute_to_class(self, cls: Any, obj_name: str) -> None:
-        """ Adds atrributes to classes """
-        cls._meta = self
+        """Adds attributes to classes.
+
+        This method adds the `_meta` attribute to the class and assigns
+        it to the current instance. It also sets the `model` attribute of
+        the instance to the class. Additionally, it sets the `db_table`
+        attribute based on the `meta` attribute of the instance or defaults
+        to the lowercase name of the class.
+
+        Args:
+            cls (Any): The class to which attributes are being added.
+            obj_name (str): The name of the object.
+
+        """
+        setattr(cls, obj_name, self)
         self.model = cls
 
         db_table = self.meta['db_table'] if self.meta else cls.__name__.lower()
         self.db_table = db_table
 
     def add_field(self, field: Any):
-        """ Adds database column """
+        """Adds a database column.
+
+        This method inserts the given field into the `local_fields` list
+        while maintaining order. It also calls the `setup_pk` method to
+        handle primary key setup. If the field represents a relationship,
+        it is also inserted into the `relation_fields` list in order.
+
+        Args:
+            field (Any): The database field to be added.
+
+        """
         bisect.insort(self.local_fields, field)
         self.setup_pk(field)
         if field.is_relation:
@@ -30,28 +66,82 @@ class MetaOptions:
 
 
     def setup_pk(self, field: Any):
-        """ Setup the primary key """
+        """Sets up the primary key for the model.
+
+        This method ensures that the model has only one primary key. If the
+        model already has a primary key and another field is marked as the
+        primary key, an error is raised. If the model does not have a primary
+        key and the field is marked as the primary key, the field is set as
+        the primary key.
+
+        Args:
+            field (Any): The database field to be checked and potentially
+                set as the primary key.
+
+        Raises:
+            ValueError: If the model already has a primary key and another
+                field is marked as the primary key.
+        """
         if self.primary_key and field.primary_key:
-            # TODO raise error
             raise ValueError('Model cannot have two pk!')
         elif not self.primary_key and field.primary_key:
             self.primary_key = field
 
     def _prepare(self, model: Any):
+        """Prepares the model for database interaction.
+
+        This method checks if the model has a primary key defined. If not,
+        it automatically adds an `AutoField` as the primary key and a
+        corresponding field with default value None to the model.
+
+        Args:
+            model (Any): The model to be prepared for database interaction.
+        """
         if not self.primary_key:
-            obj_name = self.model.__name__.lower() + '_id'
-            self.model.add_to_class(obj_name, AutoField(primary_key=True))
-            self.model.__fields__[obj_name] = Field(default=None)
+            obj_name = model.__name__.lower() + '_id'
+            model.add_to_class(obj_name, AutoField(primary_key=True))
+            model.__fields__[obj_name] = Field(default=None)
 
     def relation_fields_name(self):
+        """Returns the names of the fields representing relationships.
+
+        This method retrieves the names of the fields in the `relation_fields`
+        list, which represent relationships with other models.
+
+        Returns:
+            List[str]: A list containing the names of the fields representing
+                relationships.
+        """
         return [field.name for field in self.relation_fields]
 
     def get_relation_field(self, name: str) -> Any:
+        """Retrieves a field representing a relationship by its name or column name.
+
+        This method searches for a field representing a relationship within the
+        `relation_fields` list by comparing either the field name or the column
+        name with the provided `name`. If a matching field is found, it is returned.
+
+        Args:
+            name (str): The name or column name of the field representing the
+                relationship.
+
+        Returns:
+            Any: The field representing the relationship if found, otherwise None.
+        """
         for field in self.relation_fields:
-            if field.name == name or field.db_column == name:
+            if field.name == name or field.column == name:
                 return field
         return None
 
     def relation_fields_column(self):
-        return [field.db_column for field in self.relation_fields]
+        """Returns the column names of the fields representing relationships.
+
+        This method retrieves the column names of the fields in the
+        `relation_fields` list, which represent relationships with other models.
+
+        Returns:
+            List[str]: A list containing the column names of the fields
+                representing relationships.
+        """
+        return [field.column for field in self.relation_fields]
 
