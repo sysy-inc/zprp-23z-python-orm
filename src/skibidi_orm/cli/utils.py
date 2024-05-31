312336d47e44e8269f9ce1cd0c9af306fd4eb1e9
jedrzej-grabski <grabski.dev@gmail.com>
2024-05-31 13:10:16 +0200

    Added methods to find and to load a schema module from a specified path.

--- src/skibidi_orm/cli/utils.py
+++ src/skibidi_orm/cli/utils.py
@@ -1,5 +1,8 @@
 import glob
+import os
 from skibidi_orm.exceptions.cli_exceptions import MultipleSchemaFilesError
+import importlib.util
+import sys
 
 GLOB_ALL_SCHEMAS = "**/**/schema.py"
 
@@ -16,3 +19,43 @@ def find_schema_file():
     if len(files) > 1:
         raise MultipleSchemaFilesError("Multiple schema files found.")
     return files.pop()
+
+
+def find_schema_file_with_a_given_path(path: str):
+    """
+    Find the schema file under the given path and return it in a .
+
+    If no schema file is found, raise FileNotFoundError.
+    """
+
+    if os.path.exists(path):
+        return path
+    else:
+        raise FileNotFoundError("No schema file found.")
+
+
+def load_schema_from_path(path: str):
+    """
+    Load the schema file from the given path into the workspace.
+
+    If schema file is not found, raise FileNotFoundError, if it cannot be imported, raise ImportError.
+    """
+
+    path = os.path.abspath(path)
+
+    tmp_cwd = os.getcwd()
+    os.chdir(os.path.dirname(path))
+
+    path = find_schema_file_with_a_given_path(path)
+
+    try:
+        spec = importlib.util.spec_from_file_location("schema", path)
+        module = importlib.util.module_from_spec(spec)  # type: ignore
+
+        sys.modules["schema"] = module
+        spec.loader.exec_module(module)  # type: ignore
+
+    except ImportError:
+        raise ImportError("Error while importing the schema file.")
+
+    os.chdir(tmp_cwd)
