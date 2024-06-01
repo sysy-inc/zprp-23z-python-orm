a0c4596166cd40a788946f586a2e8324ad439b27
xDepcio <olek.drwal@gmail.com>
2024-06-01 13:44:02 +0200

    Made PostgresConstraintConverter fit Postgres

--- src/skibidi_orm/migration_engine/converters/postgres/constraints.py
+++ src/skibidi_orm/migration_engine/converters/postgres/constraints.py
@@ -11,34 +11,34 @@ from skibidi_orm.migration_engine.adapters.database_objects.constraints import (
 from typing import cast
 
 
-class SQLite3ConstraintConverter(ConstraintSQLConverter):
-    """Class responsible for converting constraint objects to raw SQLite3 SQL strings"""
+class PostgresConstraintConverter(ConstraintSQLConverter):
+    """Class responsible for converting constraint objects to raw Postgres SQL strings"""
 
     @staticmethod
     def convert_constraint_change_operation_to_SQL(_: Constraint) -> str:
-        """Convert a given constraint change operation to a SQLite3 SQL string"""
+        """Convert a given constraint change operation to a Postgres SQL string"""
         raise NotImplementedError(
-            "Constraint change operations are not supported in SQLite3 yet."
+            "Constraint change operations are not supported in Postgres yet."
         )
 
     @staticmethod
     def convert_constraint_to_SQL(constraint: Constraint) -> str:
-        """Convert a given constraint to a SQLite3 SQL string"""
         if constraint.constraint_type == ConstraintType.PRIMARY_KEY:
             return "PRIMARY KEY"
         elif constraint.constraint_type == ConstraintType.UNIQUE:
             return "UNIQUE"
         elif constraint.constraint_type == ConstraintType.FOREIGN_KEY:
             constraint = cast(ForeignKeyConstraint, constraint)
-            return (
-                f"FOREIGN KEY ({', '.join(constraint.column_mapping.keys())}) REFERENCES"
-                f" {constraint.referenced_table} ({', '.join(constraint.column_mapping.values())})"
+            column_mappings = ", ".join(
+                f"{col} REFERENCES {constraint.referenced_table}({ref_col})"
+                for col, ref_col in constraint.column_mapping.items()
             )
+            return f"FOREIGN KEY ({column_mappings})"
         elif constraint.constraint_type == ConstraintType.CHECK:
             return f"CHECK ({cast(CheckConstraint, constraint).column_name} {cast(CheckConstraint, constraint).condition})"
         elif constraint.constraint_type == ConstraintType.NOT_NULL:
             return "NOT NULL"
         else:
             raise UnsupportedConstraintError(
-                f"Constraints of type {constraint.constraint_type} are not supported by SQLite3"
+                f"Constraint type {constraint.constraint_type} is not supported."
             )
