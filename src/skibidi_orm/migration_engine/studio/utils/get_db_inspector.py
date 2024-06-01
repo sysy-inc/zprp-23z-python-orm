55898566fc0607d7409f4825572994bff346b097
xDepcio <olek.drwal@gmail.com>
2024-06-01 15:09:59 +0200

    Added PostgresDataMutator

--- src/skibidi_orm/migration_engine/studio/utils/get_db_inspector.py
+++ src/skibidi_orm/migration_engine/studio/utils/get_db_inspector.py
@@ -2,6 +2,9 @@ from skibidi_orm.migration_engine.db_config.base_config import BaseDbConfig
 from skibidi_orm.migration_engine.db_config.postgres_config import PostgresConfig
 from skibidi_orm.migration_engine.db_config.sqlite3_config import SQLite3Config
 from skibidi_orm.migration_engine.db_inspectors.base_inspector import BaseDbInspector
+from skibidi_orm.migration_engine.db_inspectors.postgres_inspector import (
+    PostgresInspector,
+)
 from skibidi_orm.migration_engine.db_inspectors.sqlite3_inspector import (
     SQLite3Inspector,
 )
@@ -11,6 +14,6 @@ def get_db_inspector(db_config: BaseDbConfig) -> BaseDbInspector:
     if isinstance(db_config, SQLite3Config):
         return SQLite3Inspector()
     elif isinstance(db_config, PostgresConfig):
-        raise NotImplementedError
+        return PostgresInspector()
 
     raise NotImplementedError
