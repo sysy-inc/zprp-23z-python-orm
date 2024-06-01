04d3cac6292052e7557eb38de5d17466805fe156
Magda Twarowska <mtwarowska02@gmail.com>
2024-06-01 14:13:42 +0200

    implemented recursive foreign key

--- tests/query_engine/field/test_related_field.py
+++ tests/query_engine/field/test_related_field.py
@@ -1,17 +1,33 @@
 from skibidi_orm.query_engine.field.related_field import ForeignKey
 from skibidi_orm.query_engine.model.base import Model
-from skibidi_orm.query_engine.field.field import CharField
-from typing import Optional
+from skibidi_orm.query_engine.field.field import CharField, IntegerField
+from typing import Optional, ForwardRef
 
 
-def test_ForeignKey_fromModel():
-    class Animal(Model):
-        name: Optional[str | CharField] = CharField(default='Adam')
+def test_ForeignKey_anotherModel():
+    class Owner(Model):
+        id: Optional[int | IntegerField] = IntegerField(primary_key=True)
+        name: Optional[str | CharField] = CharField()
 
-    class Person(Model):
-        name: Optional[str | CharField] = CharField(default='Adam')
-        friend = ForeignKey(to=Animal, default=None)
+    class Dog(Model):
+        id: Optional[int | IntegerField] = IntegerField(primary_key=True)
+        name: Optional[str | CharField] = CharField('Reksio')
+        owner: Optional[Owner | ForeignKey] = ForeignKey(to=Owner)
 
-    adam = Animal()
-    basia = Person(name='Basia', friend=adam)
-    assert basia.friend is not None
+    adam = Owner(1, 'Adam')
+    dog = Dog(1, "Reksio", adam)
+    assert dog.owner == adam
+    assert dog.owner_id == 1
+
+
+def test_ForeignKey_self():
+    DogType: Optional['Dog'] = ForwardRef('Dog')
+    class Dog(Model):
+        id: Optional[int | IntegerField] = IntegerField(primary_key=True)
+        name: Optional[str | CharField] = CharField('Reksio')
+        friend: Optional[DogType | ForeignKey] = ForeignKey(to='self')
+
+    maks = Dog(1, 'Maks')
+    reks = Dog(2, "Reks", maks)
+    assert reks.friend == maks
+
